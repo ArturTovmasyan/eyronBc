805759548f16f861535d9f45c3e775092d3d61ce
aram <asimonyan87@gmail.com>
2015-09-08 17:11:38 +0400

    sign in with google

--- src/AppBundle/Entity/Goal.php
+++ src/AppBundle/Entity/Goal.php
@@ -8,15 +8,14 @@
 
 namespace AppBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="dream")
+ * @ORM\Table(name="goal")
  */
-class Dream
+class Goal
 {
     /**
      * @ORM\Id
