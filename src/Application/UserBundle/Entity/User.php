540d1d27aa5c712871ce4a6942ae7c5b77ebf48f
aram <asimonyan87@gmail.com>
2015-09-08 14:30:08 +0400

    add fos user bundle

---
+++ src/Application/UserBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aram
+ * Date: 9/8/15
+ * Time: 2:16 PM
+ */
+
+namespace Application\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
