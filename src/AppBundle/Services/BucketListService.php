4986133bc920ee75ce6ad28f0a890de99a4ed1d8
aram <asimonyan87@gmail.com>
2015-09-11 13:41:59 +0400

    add servce

---
+++ src/AppBundle/Services/BucketListService.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aram
+ * Date: 9/11/15
+ * Time: 1:29 PM
+ */
+
+namespace AppBundle\Services;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\DependencyInjection\Container;
+
+
+/**
+ * Class BucketListService
+ * @package AppBundle\Services
+ */
+class BucketListService
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\Container
+     */
+    protected  $container;
+
+    /**
+     * @var
+     */
+    protected $em;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+        $this->em = $container->get('doctrine')->getManager();
+    }
+}
