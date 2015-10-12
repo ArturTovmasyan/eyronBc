6b1029385b46f0c665362d48c6dbd6b3bfc3c2ca
Ani <animanukyan23@gmail.com>
2015-10-12 16:05:49 +0400

    file uploads

--- src/AppBundle/Resources/public/js/angular/goal.js
+++ src/AppBundle/Resources/public/js/angular/goal.js
@@ -1,10 +1,34 @@
 'use strict';
 
 angular.module('goal', [])
-    .controller('goalAdd',['$scope',function($scope){
+    .controller('goalAdd',['$scope', '$timeout', function($scope, $timeout){
 
         $('.purple input').iCheck({
             checkboxClass: 'iradio_square-grey',
             increaseArea: '20%'
         });
+
+        Dropzone.options.goalDropzone = false;
+
+        $scope.submit = function(){
+            $scope.goalDropzone.uploadFiles($scope.goalDropzone.files);
+        };
+
+        $scope.initDropzone = function(url, formSelector){
+            if(!url){
+                return;
+            }
+
+            $scope.goalDropzone = new Dropzone('#goalDropzone', {
+                url: url,
+                addRemoveLinks: true,
+                uploadMultiple: true,
+                autoProcessQueue: false,
+                completemultiple: function(){
+                    $timeout(function(){
+                        angular.element(formSelector)[0].submit();
+                    },1000);
+                }
+            });
+        }
     }]);
