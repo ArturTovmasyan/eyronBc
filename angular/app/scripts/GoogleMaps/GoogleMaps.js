bfd757ac5e9e12d96fff6304dab61517410831cd
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2015-10-13 19:41:22 +0400

    location

--- angular/app/scripts/GoogleMaps/GoogleMaps.js
+++ angular/app/scripts/GoogleMaps/GoogleMaps.js
@@ -1,53 +1,88 @@
-angular.module('GoogleMaps', [])
+angular.module('Google', [])
     .directive('simpleMapMarker',[function(){
 
-    function Initialize(el){
-        var m, data = {};
-        data.center = new google.maps.LatLng(40.177037, 44.514841);
-        data.zoom = 16;
-        data.mapTypeId = google.maps.MapTypeId.ROADMAP;
-        m = new google.maps.Map(el,data);
+        function Initialize(el){
+            var m, data = {};
+            data.center = new google.maps.LatLng(40.177037, 44.514841);
+            data.zoom = 16;
+            data.mapTypeId = google.maps.MapTypeId.ROADMAP;
+            m = new google.maps.Map(el,data);
 
-        return m;
-    }
+            return m;
+        }
 
-    return {
-        restrict: 'EA',
-        scope: {
-            markers: '='
-        },
-        compile: function compile(){
+        return {
+            restrict: 'EA',
+            scope: {
+                markers: '='
+            },
+            compile: function compile(){
 
-            function addMarker(obj, map){
+                function addMarker(obj, map){
 
-                if(!angular.isObject(obj)){
-                    return $log.error("'addMarker`s argument isn`t object");
-                }
+                    if(!angular.isObject(obj)){
+                        return $log.error("'addMarker`s argument isn`t object");
+                    }
 
-                if(!angular.isNumber(obj.latitude) || !angular.isNumber(obj.longitude)){
-                    return;
-                }
-
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(obj.latitude, obj.longitude),
-                    map: map
-                });
-
-                return marker;
-            }
-
-
-            return function(scope, el){
-                scope.markers = {};
-                scope.map = Initialize(el[0]);
-
-                scope.$watch('markers',function(d){
-                    if(!d){
+                    if(!angular.isNumber(obj.latitude) || !angular.isNumber(obj.longitude)){
                         return;
                     }
 
-                },true);
-            };
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(obj.latitude, obj.longitude),
+                        map: map
+                    });
+
+                    return marker;
+                }
+
+
+                return function(scope, el){
+                    scope.markers = {};
+                    scope.map = Initialize(el[0]);
+
+                    scope.$watch('markers',function(d){
+                        if(!d){
+                            return;
+                        }
+
+                    },true);
+                };
+            }
+        };
+    }])
+    .directive('googlePlacesAutocomplete',[function(){
+        return {
+            restrict: 'EA',
+            scope: {
+                hiddenStorage: '@'
+            },
+            compile: function(){
+                return function(scope, el){
+
+                    var autocomplete = new google.maps.places.Autocomplete(el[0],{types: ['address']});
+                    google.maps.event.addListener(autocomplete, 'place_changed', function(){
+                        var result = autocomplete.getPlace();
+                        console.log(result);
+
+                        if(!result.geometry ||
+                            !result.geometry.location ||
+                            !result.formatted_address){
+                            return;
+                        }
+
+                        var place = {};
+                        place.location = {
+                            latitude: result.geometry.location.lat(),
+                            longitude: result.geometry.location.lng()
+                        };
+
+                        place.address = result.formatted_address;
+
+                        angular.element(scope.hiddenStorage).val(JSON.stringify(place));
+                        angular.element(scope.hiddenStorage).attr('value',JSON.stringify(place));
+                    })
+                }
+            }
         }
-    };
-}]);
+    }]);
--- src/AppBundle/Resources/public/js/angular/goal.js
+++ src/AppBundle/Resources/public/js/angular/goal.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('goal', ['Interpolation',
-        'GoogleMaps',
+        'Google',
         'mgcrea.ngStrap.popover',
         'ngAnimate'
     ])
