0b56e9f996ef7ba0872863a207f82560b0db377d
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2015-10-19 17:53:29 +0400

    AngularStrap open popover functionality

--- src/AppBundle/Resources/public/js/angular/main.js
+++ src/AppBundle/Resources/public/js/angular/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-angular.module('main',['mgcrea.ngStrap.popover', 'ngAnimate', 'Components', 'ngSanitize']);
+angular.module('main',['mgcrea.ngStrap.popover',
+    'ngAnimate',
+    'Components',
+    'ngSanitize'])
+    .controller('MainController',['$scope',function($scope){
+        console.log($scope);
+
+        $scope.openSignInPopover = function(){
+            var popoverScope = angular.element(".sign-in-popover").scope().$$childHead;
+            if(!popoverScope.$isShown){
+                popoverScope.$show();
+                $scope.joinToggle2 = !$scope.joinToggle2;
+            }
+        }
+    }]);
