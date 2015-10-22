c4a8cfcfe724f679b780f2a1b542c35af85a1409
aram <asimonyan87@gmail.com>
2015-10-22 10:14:27 +0400

    same changes

--- src/Application/UserBundle/Controller/MainController.php
+++ src/Application/UserBundle/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace Application\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
@@ -15,5 +16,22 @@ class MainController extends Controller
     public function settingsAction()
     {
         return array();
+    }
+
+    /**
+     * @Route("/check-login", name="check-login")
+     * @Template()
+     */
+    public function checkLoginAction(Request $request)
+    {
+        // get referer
+        $referer = $request->server->get('HTTP_REFERER');
+        // generate url
+        $url = $referer ?
+            $referer :
+            $this->generateUrl('homepage');
+
+        $this->addFlash('', '');
+        return $this->redirect($url);
     }
 }
