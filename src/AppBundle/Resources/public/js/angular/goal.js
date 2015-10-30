85ffe19da101674935b4b82ddc3ad1ba108c692b
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2015-10-30 18:20:46 +0400

    map

--- src/AppBundle/Resources/public/js/angular/goal.js
+++ src/AppBundle/Resources/public/js/angular/goal.js
@@ -172,19 +172,4 @@ angular.module('goal', ['Interpolation',
                 }
             }
         }
-    }])
-    .animation('.slide', function() {
-        var NG_HIDE_CLASS = 'ng-hide';
-        return {
-            beforeAddClass: function(element, className, done) {
-                if(className === NG_HIDE_CLASS) {
-                    element.slideUp(done);
-                }
-            },
-            removeClass: function(element, className, done) {
-                if(className === NG_HIDE_CLASS) {
-                    element.hide().slideDown(done);
-                }
-            }
-        }
-    });
+    }]);
