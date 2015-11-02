94ead64e2b1a8e92b3d96508ba9b913307c7c51f
aram <asimonyan87@gmail.com>
2015-11-02 17:31:07 +0400

    fix

--- src/AppBundle/Command/MigrationCommand.php
+++ src/AppBundle/Command/MigrationCommand.php
@@ -9,15 +9,16 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Entity\Goal;
+use AppBundle\Entity\GoalImage;
+use AppBundle\Model\PublishAware;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Translation\Loader\JsonFileLoader;
 
 
 class MigrationCommand extends ContainerAwareCommand
 {
-
     /**
      *
      */
@@ -45,20 +46,64 @@ class MigrationCommand extends ContainerAwareCommand
         //get array from json
         $arrayData = json_decode($jsonData, true);
 
-        foreach($arrayData['results'] as $data){
-            dump($data);
-        }
+        // get entity manager
+        $em = $this->getContainer()->get('doctrine')->getManager();
 
-
-        exit;
-
-//        dump($data);
-
-        // get Entity manager
-        $em = $this->getContainer()->get("doctrine")->getManager();
-
+        $blService = $this->getContainer()->get('bl_service');
 
         $output->writeln("<info>Starting</info>");
+
+        // loop for data
+        foreach($arrayData['results'] as $data){
+
+//            $category = $data['categories_'];
+//            $didCount = $data['didCount'];
+//            $goalID = $data['goalID'];
+//            $likesCount = $data['likesCount'];
+//            $listedCount = $data['listedCount'];
+
+
+            $imageName = $data['goalImage']['name'];
+            $imagePath = $data['goalImage']['url'];
+            $imageOriginName = array_key_exists('imageName', $data) ? $data['imageName'] : 'img';
+
+            $description = array_key_exists('description', $data) ? $data['description'] : ' ';
+            $createdAt = $data['createdAt'];
+            $createdAt = substr($createdAt, 0, 10);
+            $updatedAt = $data['updatedAt'];
+            $updatedAt = substr($updatedAt, 0, 10);
+            $title = $data['title'];
+
+            $create = \DateTime::createFromFormat('Y-m-d', $createdAt);
+            $update = \DateTime::createFromFormat('Y-m-d', $updatedAt);
+
+            $goal = new Goal();
+            $goal->setTitle($title);
+            $goal->setDescription($description);
+            $goal->setCreated($create);
+            $goal->setUpdated($update);
+            $goal->setStatus(Goal::PUBLIC_PRIVACY);
+            $goal->setPublish(PublishAware::PUBLISH);
+
+            $goalImage = new GoalImage();
+            $goalImage->setFileName($imageName);
+            $goalImage->setFileOriginalName($imageOriginName);
+
+            file_put_contents($goalImage->getAbsolutePath() . '/' . $goalImage->getFileName() , file_get_contents($imagePath));
+
+            $blService->generateFileForCover($goalImage);
+            $goalImage->setCover(true);
+            $blService->generateFileForList($goalImage);
+            $goalImage->setList(true);
+
+            $goal->addImage($goalImage);
+
+            $em->persist($goal);
+            $em->persist($goalImage);
+
+        }
+
+        $em->flush();
 
         $output->writeln("<info>Success</info>");
     }
