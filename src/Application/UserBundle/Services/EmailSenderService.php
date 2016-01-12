8a31ae93f25b8ffbc8deae9430116ba92e59039a
ARTUR <artur.tovmasyan777@gmail.com>
2016-01-12 16:50:39 +0400

    fix

--- src/Application/UserBundle/Services/EmailSenderService.php
+++ src/Application/UserBundle/Services/EmailSenderService.php
@@ -35,7 +35,7 @@ class EmailSenderService
 
         $message = \Swift_Message::newInstance()
             ->setSubject('Please confirm your ' . $projectName . ' account')
-            ->setFrom('confirmEmail@'. $projectName . '.com')
+            ->setFrom('tigranparemuzian@gmail.com')
             ->setCc($email)
             ->setContentType("text/html; charset=UTF-8")
             ->setBody($this->container->get('templating')->render(
