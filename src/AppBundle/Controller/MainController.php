6f678655965cae1efc46b5ef4dd67e44afbe7aa5
Andranik <andranik.hovhann@gmail.com>
2016-01-19 19:15:00 +0400

    visible / invisible

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -97,11 +97,11 @@ class MainController extends Controller
         $this->get('bl_news_feed_service')->updateNewsFeed();
 
         //If user is logged in then show news feed
-        $query = $em->getRepository('AppBundle:NewFeed')->findNewFeedQuery($this->getUser()->getId());
+        $result = $em->getRepository('AppBundle:NewFeed')->findNewFeed($this->getUser()->getId());
 
         $paginator  = $this->get('knp_paginator');
         $pagination = $paginator->paginate(
-            $query,
+            $result,
             $request->query->getInt('page', 1)/*page number*/,
             5/*limit per page*/
         );
@@ -113,7 +113,6 @@ class MainController extends Controller
 
     /**
      * @Route("/register/confirmed", name="registration_confirmed")
-     * @Template()
      * @Security("has_role('ROLE_USER')")
      * @return array
      */
--- src/Application/UserBundle/Controller/MainController.php
+++ src/Application/UserBundle/Controller/MainController.php
@@ -94,7 +94,11 @@ class MainController extends Controller
         }
 
         $referer = $request->headers->get('referer');
-        return new RedirectResponse($referer);
+        if ($referer){
+            return new RedirectResponse($referer);
+        }
+
+        return $this->redirectToRoute('homepage');
     }
 
     /**
