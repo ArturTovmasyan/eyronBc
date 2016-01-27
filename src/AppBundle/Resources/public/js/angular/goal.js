51ca839190a85ed0370bfb2af03d692c2d07d3c0
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2016-01-27 16:09:39 +0400

    radiobuttons

--- src/AppBundle/Resources/public/js/angular/goal.js
+++ src/AppBundle/Resources/public/js/angular/goal.js
@@ -124,6 +124,15 @@ angular.module('goal', ['Interpolation',
 
                 $scope.$apply();
             });
+
+            angular.element('input.important-radio').iCheck({
+                radioClass: 'iradio_square-purple',
+                increaseArea: '20%'
+            }).on('ifChanged', function (event) {
+                var target = angular.element(event.target);
+                target.trigger('change');
+            });
+
         }, 500);
 
     }])
