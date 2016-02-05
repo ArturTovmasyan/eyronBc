997e02087455a3d25c018bfc40cb14843b26dc05
Vazgen Manukyan <vazgen.manukyan@gmail.com>
2016-02-05 14:17:37 +0400

    fixed

--- src/AppBundle/Tests/Controller/BucketListControllerTest.php
+++ src/AppBundle/Tests/Controller/BucketListControllerTest.php
@@ -3,60 +3,23 @@
 namespace AppBundle\Tests\Controller;
 
 
+use AppBundle\Entity\UserGoal;
+use Symfony\Component\HttpFoundation\Response;
+
 class BucketListControllerTest extends BaseClass
 {
 
     /**
-     *
      * @dataProvider filterProvider
      */
-    public function testMyList($filters)
+    public function testMyList($filter)
     {
         // try to open goal view page
-        $this->client->request('GET', '/user-profile/1/' . BaseClass::ACTIVE, $filters[0]);
+        $this->client->request('GET', '/user-profile/1/' . UserGoal::ACTIVE, $filter);
 
-        $this->assertEquals($this->client->getResponse()->getStatusCode(), BaseClass::HTTP_STATUS_OK, 'can not open goal view page!');
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_OK, 'can not open goal view page!');
 
-        if ($profile = $this->client->getProfile()){
-        // check the number of requests
-        $this->assertLessThan(10, $profile->getCollector('db')->getQueryCount(), "number of requests are much more greater than needed on group list page!");
-        }
-
-
-        // try to open goal view page
-        $this->client->request('GET', '/user-profile/1/' . BaseClass::ACTIVE, $filters[1]);
-
-        $this->assertEquals($this->client->getResponse()->getStatusCode(), BaseClass::HTTP_STATUS_OK, 'can not open goal view page!');
-
-        if ($profile = $this->client->getProfile()){
-            // check the number of requests
-            $this->assertLessThan(10, $profile->getCollector('db')->getQueryCount(), "number of requests are much more greater than needed on group list page!");
-        }
-
-        // try to open goal view page
-        $this->client->request('GET', '/user-profile/1/' . BaseClass::ACTIVE, $filters[2]);
-
-        $this->assertEquals($this->client->getResponse()->getStatusCode(), BaseClass::HTTP_STATUS_OK, 'can not open goal view page!');
-
-        if ($profile = $this->client->getProfile()){
-            // check the number of requests
-            $this->assertLessThan(10, $profile->getCollector('db')->getQueryCount(), "number of requests are much more greater than needed on group list page!");
-        }
-        // try to open goal view page
-        $this->client->request('GET', '/user-profile/1/' . BaseClass::ACTIVE, $filters[3]);
-
-        $this->assertEquals($this->client->getResponse()->getStatusCode(), BaseClass::HTTP_STATUS_OK, 'can not open goal view page!');
-
-        if ($profile = $this->client->getProfile()){
-            // check the number of requests
-            $this->assertLessThan(10, $profile->getCollector('db')->getQueryCount(), "number of requests are much more greater than needed on group list page!");
-        }
-        // try to open goal view page
-        $this->client->request('GET', '/user-profile/1/' . BaseClass::ACTIVE, $filters[4]);
-
-        $this->assertEquals($this->client->getResponse()->getStatusCode(), BaseClass::HTTP_STATUS_OK, 'can not open goal view page!');
-
-        if ($profile = $this->client->getProfile()){
+        if ($profile = $this->client->getProfile()) {
             // check the number of requests
             $this->assertLessThan(10, $profile->getCollector('db')->getQueryCount(), "number of requests are much more greater than needed on group list page!");
         }
