c1554f7c19bbf957e2c356aa0648f427ee1a6c9c
ARTUR <artur.tovmasyan777@gmail.com>
2016-02-25 15:13:21 +0400

    fix locale change in admin

--- src/AppBundle/Listener/LocaleListener.php
+++ src/AppBundle/Listener/LocaleListener.php
@@ -39,16 +39,23 @@ class LocaleListener implements EventSubscriberInterface
         // get request
         $request = $event->getRequest();
 
-        if (!$request->hasPreviousSession()) {
-            return;
-        }
+        //get current url
+        $currentUrl = $request->getUri();
 
-        // try to see if the locale has been set as a _locale routing parameter
-        if ($locale = $request->attributes->get('_locale')) {
-            $request->getSession()->set('_locale', $locale);
-        } else {
-            // if no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        //check if current url is not admin
+        if (strpos($currentUrl, 'admin') == false) {
+
+            if (!$request->hasPreviousSession()) {
+                return;
+            }
+
+            // try to see if the locale has been set as a _locale routing parameter
+            if ($locale = $request->attributes->get('_locale')) {
+                $request->getSession()->set('_locale', $locale);
+            } else {
+                // if no explicit locale has been set on this request, use one from the session
+                $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+            }
         }
     }
 
