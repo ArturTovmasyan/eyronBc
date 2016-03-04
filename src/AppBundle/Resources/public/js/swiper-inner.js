cc701cfe55e2e8854a6df27002349d4354cb8ca8
Hazarapet <hazarapettunanyan@gmail.com>
2016-03-04 18:27:08 +0400

    image reponsive

--- src/AppBundle/Resources/public/js/swiper-inner.js
+++ src/AppBundle/Resources/public/js/swiper-inner.js
@@ -2,12 +2,14 @@
 
 $(document).ready(function(){
 
+    // Main
     var main_swiper = new Swiper('#main-slider', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
-        spaceBetween: 30
+        spaceBetween: 30,
+        autoplay: 3000
     });
 
     var main_swiper_video = new Swiper('#main-slider-video', {
@@ -15,7 +17,27 @@ $(document).ready(function(){
         paginationClickable: true,
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
-        spaceBetween: 30
+        spaceBetween: 30,
+        autoplay: 3000
     });
+
+    // Story
+    var story_image_slider = [];
+    $(".story-slider").each(function(k, v){
+        var id = $(v).attr('id');
+
+        var tmp = new Swiper('#'+id, {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            slidesPerView: 2,
+            freeMode: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            spaceBetween: 30
+        });
+
+        story_image_slider.push(tmp);
+    });
+
 
 });
