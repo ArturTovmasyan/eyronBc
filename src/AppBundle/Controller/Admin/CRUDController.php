a733d2756f430f721ae8813bbd7d08490e588dde
ARTUR <artur.tovmasyan777@gmail.com>
2016-05-05 20:14:38 +0400

    fix in merge action

--- src/AppBundle/Controller/Admin/CRUDController.php
+++ src/AppBundle/Controller/Admin/CRUDController.php
@@ -10,38 +10,49 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Entity\Goal;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CRUDController extends Controller
 {
 
     /**
-     * @param Goal $goal
-     * @ParamConverter("goal", class="AppBundle:Goal")
      * @return \Symfony\Component\HttpFoundation\Response
      */
 
-    public function mergeAction(Goal $goal, $mergeId = null)
+    public function mergeAction(Request $request)
     {
+        //get current goal
         $object = $this->admin->getSubject();
+
+        //get entity manager
+        $em = $this->get('doctrine')->getEntityManager();
 
         if (!$object) {
             throw new NotFoundHttpException(sprintf('unable to find the object with id : test'));
         }
 
+//        // create goal form
+//        $form = $this->createForm(new GoalSelectType);
+//
+//        // check request method
+//        if($request->isMethod("POST")) {
+//
+//            // get data from request
+//            $form->handleRequest($request);
+//
+//            // check valid
+//            if ($form->isValid()) {
+//
+//            }
+//        }
 
-//        // Be careful, you may need to overload the __clone method of your object
-//        // to set its id to null !
-//        $clonedObject = clone $object;
-//
-//        $clonedObject->setName($object->getName().' (Clone)');
-//
-//        $this->admin->create($clonedObject);
-//
-//        $this->addFlash('sonata_flash_success', 'Cloned successfully');
+        //get all goal
+        $allGoals = $em->getRepository('AppBundle:Goal')->findAll();
 
         return $this->render('AppBundle:Admin:goal_merge.html.twig', array(
-          'mergeGoal' => $goal
+          'mergeGoal' => $object, 'allGoal' => $allGoals
         ));
     }
 
