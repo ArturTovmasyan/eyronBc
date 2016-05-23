d849eaf34892fde191baa233489e65b4aa599eee
gevor <gsmgevor@rambler.ru>
2016-05-23 18:07:54 +0400

    fix ng nresors removing

--- src/AppBundle/Resources/public/js/merge-goal-image.js
+++ src/AppBundle/Resources/public/js/merge-goal-image.js
@@ -4,7 +4,10 @@ $(document).ready(function()
         $("#app_bundle_merge_goal_goal").change(function(element){
             var id = element.val;
             $.get( "/api/v1.0/goals/image/"+id, function( data ) {
+
+                $('#merge-goal-id').text("ID: " + id);
                 $('#merge_image').attr("src",data.image_path);
+                $('#merge_image').show();
             });
         })
     }, 500);
