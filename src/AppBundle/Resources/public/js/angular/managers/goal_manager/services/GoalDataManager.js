60e8c351a90f3a34d3d5dda0c4d7e9d06670978b
gevor <gsmgevor.gs@gmail.com>
2016-06-13 12:53:04 +0400

    fix analitic module

--- src/AppBundle/Resources/public/js/angular/managers/goal_manager/services/GoalDataManager.js
+++ src/AppBundle/Resources/public/js/angular/managers/goal_manager/services/GoalDataManager.js
@@ -1,11 +1,23 @@
 'use strict';
 
-angular.module('goal')
-  .service('UserGoalDataManager', ['$resource', 'envPrefix', function($resource, envPrefix){
+angular.module('goal', ['angulartics', 'angulartics.google.analytics'])
+  .service('UserGoalDataManager', ['$resource', 'envPrefix', '$analytics', function($resource, envPrefix, $analytics){
     return $resource( envPrefix + 'api/v1.0/usergoals/:id/:where/:what', {}, {
-      addGoal: {method:'GET', params:{where: 'dones'}},
-      editGoal: {method:'PUT'},
-      doneGoal: {method:'GET', params:{where: 'dones', what: true }},
-      deleteUserGoal: {method:'DELETE'}
+      addGoal: {method:'GET', params:{where: 'dones'}, transformResponse: function (object) {
+          $analytics.eventTrack('Goal add', {  category: 'Goal', label: 'Goal add from Web' });
+          return object;
+      }},
+      editGoal: {method:'PUT', transformResponse: function (object) {
+          $analytics.eventTrack('Goal add', {  category: 'Goal', label: 'Goal add from Web' });
+          return object;
+      }},
+      doneGoal: {method:'GET', params:{where: 'dones', what: true }, transformResponse: function (object) {
+          $analytics.eventTrack('Goal add', {  category: 'Goal', label: 'Goal add from Web' });
+          return object;
+      }},
+      deleteUserGoal: {method:'DELETE', transformResponse: function (object) {
+          $analytics.eventTrack('Goal add', {  category: 'Goal', label: 'Goal add from Web' });
+          return object;
+      }}
     });
   }]);
