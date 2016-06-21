1776a545dc9066f30eb7398d2d0670bccf46f0b8
Andranik <andranik.hovhann@gmail.com>
2016-06-21 17:52:54 +0400

    ignore empty success stories

--- src/AppBundle/Listener/DoctrineListener.php
+++ src/AppBundle/Listener/DoctrineListener.php
@@ -211,7 +211,7 @@ class DoctrineListener
 
                 $goal = $entity->getGoal();
             }
-            elseif($entity instanceof SuccessStory){
+            elseif($entity instanceof SuccessStory && str_replace(" ", "", $entity) != ""){
                 $action = NewFeed::SUCCESS_STORY;
                 $goal = $entity->getGoal();
                 $story = $entity;
