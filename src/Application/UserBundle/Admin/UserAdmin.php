67ad5effbd496874c79103787d24b89bd3e9df16
ARTUR <artur.tovmasyan777@gmail.com>
2016-06-28 11:20:30 +0400

    fix missing translations and icons on user admin page

--- src/Application/UserBundle/Admin/UserAdmin.php
+++ src/Application/UserBundle/Admin/UserAdmin.php
@@ -30,12 +30,13 @@ class UserAdmin extends AbstractAdmin
     protected function configureShowFields(ShowMapper $showMapper)
     {
         $showMapper
-            ->add('id', null, array('label' =>' admin.label.name.id'))
+            ->add('id', null, array('label' =>'admin.label.name.id'))
             ->add('email', null, array('label' => 'admin.label.name.email'))
             ->add('firstName', null, array('label'=>'admin.label.name.firstName'))
             ->add('lastName', null, array('label' => 'admin.label.name.lastName'))
             ->add('picture', null, array('template' => 'ApplicationUserBundle:Admin:user_show_picture.html.twig'))
             ->add('profile', null, array('template' => 'ApplicationUserBundle:Admin:user_show_profile_link.html.twig'))
+            ->add('userSocial', null, array('template' => 'ApplicationUserBundle:Admin:user_social_icon_show.html.twig'))
             ->add('listedGoals', null, array('template' => 'ApplicationUserBundle:Admin:user_show_listed_goal_count.html.twig'))
             ->add('createdGoals', null, array('template' => 'ApplicationUserBundle:Admin:user_show_created_goal.html.twig'))
             ->add('successStory count', null, array('template' => 'ApplicationUserBundle:Admin:user_show_goal_story.html.twig'))
