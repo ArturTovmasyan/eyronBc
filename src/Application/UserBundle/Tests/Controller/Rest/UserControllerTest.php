c125081a903c154102b8014a6d003ad9eebe82a3
ARTUR <artur.tovmasyan777@gmail.com>
2016-07-01 15:38:59 +0400

    fix in user test

--- src/Application/UserBundle/Tests/Controller/Rest/UserControllerTest.php
+++ src/Application/UserBundle/Tests/Controller/Rest/UserControllerTest.php
@@ -52,190 +52,190 @@ class UserControllerTest extends WebTestCase
         $this->clientAuthorized->enableProfiler();
     }
 
-//    /**
-//     * This function is used to check Post function in rest
-//     */
-//    public function testPostBlankEmail()
-//    {
-//        $url = '/api/v1.0/users';
-//
-//        // try to register new user
-//        $this->client->request('POST', $url,
-//            array(
-//                'email' => '',
-//                'plainPassword' => 'Test1234',
-//                'firstName' => 'Test',
-//                'lastName' => 'Testyan',
-//                'birthday' => '01/12/1990',
-//            )
-//        );
-//
-//        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "can not register new user with blank email in postAction rest!");
-//
-//        $this->assertTrue(
-//            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->client->getResponse()->headers
-//        );
-//    }
-//
-//    /**
-//     * This function is used to check Post function in rest
-//     */
-//    public function testPostBlankPassword()
-//    {
-//        $url = '/api/v1.0/users';
-//
-//        // try to register new user
-//        $this->client->request('POST', $url,
-//            array(
-//                'email' => 'test@test.com',
-//                'plainPassword' => '',
-//                'firstName' => 'Test',
-//                'lastName' => 'Testyan',
-//                'birthday' => '01/12/1990',
-//            )
-//        );
-//
-//        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank password in postAction(user registration) rest!");
-//
-//        $this->assertTrue(
-//            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->client->getResponse()->headers
-//        );
-//    }
-//
-//    /**
-//     * This function is used to check Post function in rest
-//     */
-//    public function testPostInvalidPasswordMinLength()
-//    {
-//        $url = '/api/v1.0/users';
-//
-//        // try to register new user
-//        $this->client->request('POST', $url,
-//            array(
-//                'email' => 'test@test.com',
-//                'plainPassword' => 'Test',
-//                'firstName' => 'Test',
-//                'lastName' => 'Testyan',
-//                'birthday' => '01/12/1990',
-//            )
-//        );
-//
-//        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with invalid password min length in postAction(user registration) rest!");
-//
-//        $this->assertTrue(
-//            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->client->getResponse()->headers
-//        );
-//    }
-//
-//    /**
-//     * This function is used to check Post function in rest
-//     */
-//    public function testPostBlankFirstName()
-//    {
-//        $url = '/api/v1.0/users';
-//
-//        // try to register new user
-//        $this->client->request('POST', $url,
-//            array(
-//                'email' => 'test@test.com',
-//                'plainPassword' => 'Test1234',
-//                'firstName' => '',
-//                'lastName' => 'Testyan',
-//                'birthday' => '01/12/1990',
-//            )
-//        );
-//
-//        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank firstName in postAction(user registration) rest!");
-//
-//        $this->assertTrue(
-//            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->client->getResponse()->headers
-//        );
-//    }
-//
-//    /**
-//     * This function is used to check Post function in rest
-//     */
-//    public function testPostBlankLastName()
-//    {
-//        $url = '/api/v1.0/users';
-//
-//        // try to register new user
-//        $this->client->request('POST', $url,
-//            array(
-//                'email' => 'test@test.com',
-//                'plainPassword' => 'Test1234',
-//                'firstName' => 'Test',
-//                'lastName' => '',
-//                'birthday' => '01/12/1990',
-//            )
-//        );
-//
-//        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank lastName in postAction(user registration) rest!");
-//
-//        $this->assertTrue(
-//            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->client->getResponse()->headers
-//        );
-//    }
-//
-//    /**
-//     * This function is used to check Post function in rest
-//     */
-//    public function testPostBlankBirthday()
-//    {
-//        $url = '/api/v1.0/users';
-//
-//        // try to register new user
-//        $this->client->request('POST', $url,
-//            array(
-//                'email' => 'test@test.com',
-//                'plainPassword' => 'Test1234',
-//                'firstName' => 'Test',
-//                'lastName' => 'Testyan',
-//                'birthday' => '',
-//            )
-//        );
-//
-//        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank birthday in postAction(user registration) rest!");
-//
-//        $this->assertTrue(
-//            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->client->getResponse()->headers
-//        );
-//    }
-//
-//    /**
-//     * This function is used to check Post function in rest
-//     */
-//    public function testPostAlreadyRegisteredEmail()
-//    {
-//        // get user
-//        $user = $this->em->getRepository('ApplicationUserBundle:User')->findOneByEmail('admin@admin.com');
-//
-//        $url = '/api/v1.0/users';
-//
-//        // try to register new user
-//        $this->client->request('POST', $url,
-//            array(
-//                'email' => $user->getEmail(),
-//                'plainPassword' => 'Test1234',
-//                'firstName' => 'Test',
-//                'lastName' => 'Testyan',
-//                'birthday' => '01/12/1990',
-//            )
-//        );
-//
-//        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with already registered email in postAction(user registration) rest!");
-//
-//        $this->assertTrue(
-//            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->client->getResponse()->headers
-//        );
-//    }
+    /**
+     * This function is used to check Post function in rest
+     */
+    public function testPostBlankEmail()
+    {
+        $url = '/api/v1.0/users';
+
+        // try to register new user
+        $this->client->request('POST', $url,
+            array(
+                'email' => '',
+                'plainPassword' => 'Test1234',
+                'firstName' => 'Test',
+                'lastName' => 'Testyan',
+                'birthday' => '01/12/1990',
+            )
+        );
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "can not register new user with blank email in postAction rest!");
+
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->client->getResponse()->headers
+        );
+    }
+
+    /**
+     * This function is used to check Post function in rest
+     */
+    public function testPostBlankPassword()
+    {
+        $url = '/api/v1.0/users';
+
+        // try to register new user
+        $this->client->request('POST', $url,
+            array(
+                'email' => 'test@test.com',
+                'plainPassword' => '',
+                'firstName' => 'Test',
+                'lastName' => 'Testyan',
+                'birthday' => '01/12/1990',
+            )
+        );
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank password in postAction(user registration) rest!");
+
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->client->getResponse()->headers
+        );
+    }
+
+    /**
+     * This function is used to check Post function in rest
+     */
+    public function testPostInvalidPasswordMinLength()
+    {
+        $url = '/api/v1.0/users';
+
+        // try to register new user
+        $this->client->request('POST', $url,
+            array(
+                'email' => 'test@test.com',
+                'plainPassword' => 'Test',
+                'firstName' => 'Test',
+                'lastName' => 'Testyan',
+                'birthday' => '01/12/1990',
+            )
+        );
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with invalid password min length in postAction(user registration) rest!");
+
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->client->getResponse()->headers
+        );
+    }
+
+    /**
+     * This function is used to check Post function in rest
+     */
+    public function testPostBlankFirstName()
+    {
+        $url = '/api/v1.0/users';
+
+        // try to register new user
+        $this->client->request('POST', $url,
+            array(
+                'email' => 'test@test.com',
+                'plainPassword' => 'Test1234',
+                'firstName' => '',
+                'lastName' => 'Testyan',
+                'birthday' => '01/12/1990',
+            )
+        );
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank firstName in postAction(user registration) rest!");
+
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->client->getResponse()->headers
+        );
+    }
+
+    /**
+     * This function is used to check Post function in rest
+     */
+    public function testPostBlankLastName()
+    {
+        $url = '/api/v1.0/users';
+
+        // try to register new user
+        $this->client->request('POST', $url,
+            array(
+                'email' => 'test@test.com',
+                'plainPassword' => 'Test1234',
+                'firstName' => 'Test',
+                'lastName' => '',
+                'birthday' => '01/12/1990',
+            )
+        );
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank lastName in postAction(user registration) rest!");
+
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->client->getResponse()->headers
+        );
+    }
+
+    /**
+     * This function is used to check Post function in rest
+     */
+    public function testPostBlankBirthday()
+    {
+        $url = '/api/v1.0/users';
+
+        // try to register new user
+        $this->client->request('POST', $url,
+            array(
+                'email' => 'test@test.com',
+                'plainPassword' => 'Test1234',
+                'firstName' => 'Test',
+                'lastName' => 'Testyan',
+                'birthday' => '',
+            )
+        );
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with blank birthday in postAction(user registration) rest!");
+
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->client->getResponse()->headers
+        );
+    }
+
+    /**
+     * This function is used to check Post function in rest
+     */
+    public function testPostAlreadyRegisteredEmail()
+    {
+        // get user
+        $user = $this->em->getRepository('ApplicationUserBundle:User')->findOneByEmail('admin@admin.com');
+
+        $url = '/api/v1.0/users';
+
+        // try to register new user
+        $this->client->request('POST', $url,
+            array(
+                'email' => $user->getEmail(),
+                'plainPassword' => 'Test1234',
+                'firstName' => 'Test',
+                'lastName' => 'Testyan',
+                'birthday' => '01/12/1990',
+            )
+        );
+
+        $this->assertEquals($this->client->getResponse()->getStatusCode(), Response::HTTP_BAD_REQUEST, "form submit with already registered email in postAction(user registration) rest!");
+
+        $this->assertTrue(
+            $this->client->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->client->getResponse()->headers
+        );
+    }
 
     /**
      * This function is used to check Post function in rest
@@ -316,66 +316,66 @@ class UserControllerTest extends WebTestCase
         }
     }
 
-//    /**
-//     * This function is used to check postLogin function in rest
-//     */
-//    public function testPostLogin()
-//    {
-//        $url = '/api/v1.0/users/logins';
-//
-//        // try to login
-//        $this->clientAuthorized->request('POST', $url,
-//            array(
-//                'username' => 'admin@admin.com',
-//                'password' => 'Test1234',
-//            )
-//        );
-//
-//        // Assert that the response status code is 2xx
-//        $this->assertTrue($this->clientAuthorized->getResponse()->isSuccessful(), "can not login in postLoginAction rest!");
-//
-//        $this->assertTrue(
-//            $this->clientAuthorized->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->clientAuthorized->getResponse()->headers
-//        );
-//    }
-//
-//    /**
-//     * This function is used to check getRegistered function in rest
-//     */
-//    public function testGetRegistered()
-//    {
-//        // get user
-//        $user = $this->em->getRepository('ApplicationUserBundle:User')->findOneByEmail('admin@admin.com');
-//
-//        $url = sprintf('/api/v1.0/users/%s/registered', $user->getEmail());
-//
-//        // try to get user registered status
-//        $this->clientAuthorized->request('GET', $url);
-//
-//        // Assert that the response status code is 2xx
-//        $this->assertTrue($this->clientAuthorized->getResponse()->isSuccessful(), "can not get user registered status in getRegisteredAction rest!");
-//
-//        // assert that user registered status is true
-//        $this->assertContains(
-//            'true',
-//            $this->clientAuthorized->getResponse()->getContent()
-//        );
-//
-//        $this->assertTrue(
-//            $this->clientAuthorized->getResponse()->headers->contains('Content-Type', 'application/json'),
-//            $this->clientAuthorized->getResponse()->headers
-//        );
-//
-//        $url = sprintf('/api/v1.0/users/%s/registered', 'nonExistentEmail');
-//
-//        // try to get user registered status
-//        $this->clientAuthorized->request('GET', $url);
-//
-//        // assert that user registered status is false
-//        $this->assertContains(
-//            'false',
-//            $this->clientAuthorized->getResponse()->getContent()
-//        );
-//    }
+    /**
+     * This function is used to check postLogin function in rest
+     */
+    public function testPostLogin()
+    {
+        $url = '/api/v1.0/users/logins';
+
+        // try to login
+        $this->clientAuthorized->request('POST', $url,
+            array(
+                'username' => 'admin@admin.com',
+                'password' => 'Test1234',
+            )
+        );
+
+        // Assert that the response status code is 2xx
+        $this->assertTrue($this->clientAuthorized->getResponse()->isSuccessful(), "can not login in postLoginAction rest!");
+
+        $this->assertTrue(
+            $this->clientAuthorized->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->clientAuthorized->getResponse()->headers
+        );
+    }
+
+    /**
+     * This function is used to check getRegistered function in rest
+     */
+    public function testGetRegistered()
+    {
+        // get user
+        $user = $this->em->getRepository('ApplicationUserBundle:User')->findOneByEmail('admin@admin.com');
+
+        $url = sprintf('/api/v1.0/users/%s/registered', $user->getEmail());
+
+        // try to get user registered status
+        $this->clientAuthorized->request('GET', $url);
+
+        // Assert that the response status code is 2xx
+        $this->assertTrue($this->clientAuthorized->getResponse()->isSuccessful(), "can not get user registered status in getRegisteredAction rest!");
+
+        // assert that user registered status is true
+        $this->assertContains(
+            'true',
+            $this->clientAuthorized->getResponse()->getContent()
+        );
+
+        $this->assertTrue(
+            $this->clientAuthorized->getResponse()->headers->contains('Content-Type', 'application/json'),
+            $this->clientAuthorized->getResponse()->headers
+        );
+
+        $url = sprintf('/api/v1.0/users/%s/registered', 'nonExistentEmail');
+
+        // try to get user registered status
+        $this->clientAuthorized->request('GET', $url);
+
+        // assert that user registered status is false
+        $this->assertContains(
+            'false',
+            $this->clientAuthorized->getResponse()->getContent()
+        );
+    }
 }
