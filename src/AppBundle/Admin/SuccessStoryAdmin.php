50cba607cbe1ba9ea08d62355d72e238c9d22004
Andranik <andranik.hovhann@gmail.com>
2016-07-07 14:47:51 +0400

    moderator asdmins

---
+++ src/AppBundle/Admin/SuccessStoryAdmin.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class SuccessStoryAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('created')
+            ->add('updated')
+            ->add('story')
+            ->add('videoLink')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('created')
+            ->add('updated')
+            ->add('story')
+            ->add('videoLink')
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('id')
+            ->add('created')
+            ->add('updated')
+            ->add('story')
+            ->add('videoLink')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('created')
+            ->add('updated')
+            ->add('story')
+            ->add('videoLink')
+        ;
+    }
+}
---
+++ src/Application/CommentBundle/Admin/CommentAdmin.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Application\CommentBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class CommentAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('body')
+            ->add('ancestors')
+            ->add('depth')
+            ->add('createdAt')
+            ->add('state')
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('body')
+            ->add('ancestors')
+            ->add('depth')
+            ->add('createdAt')
+            ->add('state')
+            ->add('id')
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('body')
+            ->add('ancestors')
+            ->add('depth')
+            ->add('createdAt')
+            ->add('state')
+            ->add('id')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('body')
+            ->add('ancestors')
+            ->add('depth')
+            ->add('createdAt')
+            ->add('state')
+            ->add('id')
+        ;
+    }
+}
