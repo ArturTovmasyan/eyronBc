1593b47136a5884b2ed1d9caece51f8997b5c1ae
Andranik <andranik.hovhann@gmail.com>
2016-07-21 18:30:29 +0400

    under maintenance feature

--- src/AppBundle/Listener/RequestListener.php
+++ src/AppBundle/Listener/RequestListener.php
@@ -10,6 +10,7 @@ namespace AppBundle\Listener;
 
 use AppBundle\Controller\Rest\MainRestController;
 use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -23,6 +24,9 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
  */
 class RequestListener //implements EventSubscriberInterface
 {
+    private $maintenance;
+    private $kernel;
+    private $templating;
     private $defaultLocale;
     private $mandatoryVersions;
     private $tokenStorage;
@@ -33,14 +37,21 @@ class RequestListener //implements EventSubscriberInterface
      * @param string $defaultLocale
      * @param $iosMandatoryVersion
      * @param $androidMandatoryVersion
-     * @param $tokenStorage
-     * @param $entityManager
+     * @param $maintenance
+     * @param \AppKernel $kernel
+     * @param TwigEngine $templating
+     * @param TokenStorage $tokenStorage
+     * @param EntityManager $entityManager
      */
-    public function __construct($defaultLocale = "en", $iosMandatoryVersion, $androidMandatoryVersion, TokenStorage $tokenStorage, EntityManager $entityManager)
+    public function __construct($defaultLocale = "en", $iosMandatoryVersion, $androidMandatoryVersion, $maintenance,
+                                \AppKernel $kernel, TwigEngine $templating, TokenStorage $tokenStorage, EntityManager $entityManager)
     {
         $this->defaultLocale = $defaultLocale;
         $this->tokenStorage  = $tokenStorage;
         $this->em            = $entityManager;
+        $this->maintenance   = $maintenance;
+        $this->kernel        = $kernel;
+        $this->templating    = $templating;
 
         $this->mandatoryVersions = [
             MainRestController::IOS_REQUEST_PARAM     => $iosMandatoryVersion,
@@ -53,15 +64,21 @@ class RequestListener //implements EventSubscriberInterface
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
-        // get request
-        $request = $event->getRequest();
+        $debug = in_array($this->kernel->getEnvironment(), array('test', 'dev'));
 
-        //get current url
+        if ($this->maintenance && !$debug) {
+            $content = $this->templating->render('::maintenance.html.twig');
+            $event->setResponse(new Response($content, 503));
+            $event->stopPropagation();
+            return;
+        }
+
+
+        $request = $event->getRequest();
         $currentUrl = $request->getUri();
 
         //check if current url is not admin
         if (strpos($currentUrl, 'admin') == false) {
-
             if (!$request->hasPreviousSession()) {
                 return;
             }
@@ -79,10 +96,8 @@ class RequestListener //implements EventSubscriberInterface
         $mobileAppPlatform = $request->query->get('mobileAppPlatform');
 
         if ($mobileAppVersion && $mobileAppPlatform){
-
             if(isset($this->mandatoryVersions[$mobileAppPlatform]) &&
-                version_compare($mobileAppVersion, $this->mandatoryVersions[$mobileAppPlatform]) == -1)
-            {
+                version_compare($mobileAppVersion, $this->mandatoryVersions[$mobileAppPlatform]) == -1){
                 $event->setResponse(new Response('You need to update your app', Response::HTTP_UPGRADE_REQUIRED));
             }
         }
