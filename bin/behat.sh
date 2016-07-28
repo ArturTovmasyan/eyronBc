f9e0a96eb104c42d4ef838f82729941811212e2a
ARTUR <artur.tovmasyan777@gmail.com>
2016-07-28 13:12:51 +0400

    fix in behat.sh file

--- bin/behat.sh
+++ bin/behat.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 rm -rf app/cache/*
+
+#app/console doctrine:database:create --env=behat
+#app/console doctrine:schema:update --force --env=behat
+#app/console doctrine:fixtures:load --env=behat
+
+app/console doctrine:database:drop --force --env=behat
 app/console doctrine:database:create --env=behat
 app/console doctrine:schema:update --force --env=behat
-app/console doctrine:fixtures:load --env=behat
+app/console doctrine:fixtures:load --env=behat --append
