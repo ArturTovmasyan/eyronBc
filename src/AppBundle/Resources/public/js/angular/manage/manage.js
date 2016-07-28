2c1a7d5fda4cab1528fb3112ce3dab69c38c945f
gevor <gsmgevor.gs@gmail.com>
2016-07-28 12:14:44 +0400

    bug fix

--- src/AppBundle/Resources/public/js/angular/manage/manage.js
+++ src/AppBundle/Resources/public/js/angular/manage/manage.js
@@ -390,4 +390,93 @@ angular.module('manage', ['Interpolation',
             }
         }
     }
-  ]);
+  ])
+  .directive('videoLink', ['$sce', function($sce){
+      return {
+          restrict: 'EA',
+          scope: {
+              array: '=',
+              key: '=',
+              link: '=',
+              limit: '='
+          },
+          templateUrl: '/bundles/app/htmls/videoLink.html',
+          link: function(scope){
+
+              scope.lm = scope.limit ? scope.limit : 3;
+
+              scope.$watch('link',function(d){
+                  if(angular.isUndefined(d)){
+                      return;
+                  }
+
+                  if(d === ''){
+                      scope.removeItem();
+                  }
+                  else {
+                      if(!scope.array[scope.key + 1] && Object.keys(scope.array).length < scope.lm){
+                          scope.array[scope.key + 1] = {};
+                      }
+                  }
+              }, true);
+
+              scope.removeItem = function(){
+                  if(scope.array[scope.array.length - 1].link){
+                      scope.array[scope.array.length] = {};
+                  }
+
+                  if(scope.key === 0){
+                      if(scope.array.length > 1){
+                          scope.array.splice(scope.key, 1);
+                      }
+                  }
+                  else {
+                      scope.array.splice(scope.key, 1);
+                  }
+              };
+
+              scope.isVideoLink = function(url){
+                  return !(!angular.isString(url) || url.indexOf("https:/") == -1);
+              };
+
+              scope.trustedUrl = function(url){
+                  return $sce.trustAsResourceUrl(url);
+              };
+          }
+      }
+  }])
+  .directive('step',[function(){
+      return {
+          restrict: 'EA',
+          scope: {
+              ngModel: '=',
+              array: '=',
+              key: '='
+          },
+          compile: function(){
+              return function(scope){
+                  scope.$watch('ngModel',function(d){
+                      if(angular.isUndefined(d)){
+                          return;
+                      }
+
+                      if(d === ''){
+                          if(scope.key === 0){
+                              if(scope.array.length > 1) {
+                                  scope.array.splice(scope.key, 1);
+                              }
+                          }
+                          else {
+                              scope.array.splice(scope.key, 1);
+                          }
+                      }
+                      else {
+                          if(!scope.array[scope.key + 1]) {
+                              scope.array[scope.key + 1] = {};
+                          }
+                      }
+                  },true);
+              }
+          }
+      }
+  }]);
