253672bd4dafc9dc0fa508380303b96ad0073731
gevor <gsmgevor.gs@gmail.com>
2016-08-05 17:35:05 +0400

    fix notification template

--- src/AppBundle/Resources/public/js/angular/modules/notification/controllers/notificationController.js
+++ src/AppBundle/Resources/public/js/angular/modules/notification/controllers/notificationController.js
@@ -44,8 +44,9 @@ angular.module('notification')
       $scope.readAll = function(){
         NotificationManager.readAll({}, function () {
           $scope.newNotCount = 0;
-          //todo something
-          $scope.allRead = true;
+          angular.forEach($scope.notifies, function (not) {
+            not.is_read = true;
+          });
         });
       };
 
@@ -61,5 +62,99 @@ angular.module('notification')
         $window.location.href = notify.notification.link;
       }
       
+    }
+  ])
+  .controller('notificationInnerController',['$scope', '$timeout', 'NotificationManager', '$compile', '$window', '$sce',
+    function ($scope, $timeout, NotificationManager, $compile, $window, $sce) {
+      $scope.busy = false;
+      $scope.notifies = [];
+      $scope.start = 0;
+      $scope.request = 0;
+      $scope.count = 10;
+      $scope.reserve = [];
+      $scope.noNotification = false;
+
+      $scope.getReserve = function() {
+        $scope.busy = $scope.noNotification;
+        $scope.notifies = $scope.notifies.concat($scope.reserve);
+        $scope.nextReserve();
+      };
+
+      $scope.nextReserve = function () {
+        if ($scope.busy) return;
+        $scope.busy = true;
+        var lastId = $scope.reserve.length?$scope.reserve[$scope.count -1].id:$scope.notifies[$scope.notifies.length -1].id;
+
+        NotificationManager.getAll({id: $scope.start,where: $scope.count, what: lastId}, function (res) {
+          if(!res.userNotifications.length){
+            $scope.noNotification = true;
+          } else {
+            $scope.reserve = res.userNotifications;
+            $scope.start += $scope.count;
+            $scope.request++;
+            $scope.busy = false;
+          }
+        });
+      };
+      
+      $scope.nextNotifications = function () {
+        if ($scope.busy) return;
+        $scope.busy = true;
+        // $scope.noItem = false;
+
+        if($scope.request){
+          $scope.getReserve();
+        } else {
+          NotificationManager.getAll({id: $scope.start,where: $scope.count}, function (res) {
+              // if get empty
+              if(!res.userNotifications.length){
+                $scope.noNotification = true;
+              } else {
+                $scope.notifies = $scope.notifies.concat(res.userNotifications);
+                $scope.start += $scope.count;
+                $scope.request++;
+                $scope.busy = false;
+                $scope.nextReserve();
+              }
+          });
+        }
+      };
+
+      $scope.nextNotifications();
+
+      $scope.bodyInHtml = function(body) {
+        return $sce.trustAsHtml(body);
+      };
+
+      $scope.delete = function(id, index){
+        NotificationManager.delete({id: id}, function () {
+          $scope.notifies.splice(index, 1);
+        });
+      };
+
+      $scope.readAll = function(){
+        NotificationManager.readAll({}, function () {
+          angular.forEach($scope.notifies, function (not) {
+            not.is_read = true;
+          });
+          if($scope.reserve.length){
+            angular.forEach($scope.reserve, function (not) {
+              not.is_read = true;
+            });
+          }
+        });
+      };
+
+      $scope.singleRead = function(id, index){
+        NotificationManager.readSingle({id: id}, function () {
+          $scope.notifies[index].is_read = true;
+        });
+      };
+
+      $scope.goNotificationPage = function (notify, index) {
+        $scope.singleRead(notify.id, index);
+        $window.location.href = notify.notification.link;
+      }
+
     }
   ]);
