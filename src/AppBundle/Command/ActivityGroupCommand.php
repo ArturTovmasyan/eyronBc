82956dcaf4553cfd09833880eba4956d22a4b100
ARTUR <artur.tovmasyan777@gmail.com>
2016-08-16 17:06:04 +0400

    create command for recalculation old activity and group it

--- src/AppBundle/Command/ActivityGroupCommand.php
+++ src/AppBundle/Command/ActivityGroupCommand.php
@@ -53,14 +53,26 @@ class ActivityGroupCommand extends ContainerAwareCommand
                         continue;
                     }
 
-                    //get date diff
-                    $dateDiff = date_diff($addNewFeed->getDateTime(), $newFeed->getDateTime(), true);
+                    $addNewFeedDate = $addNewFeed->getDateTime();
+                    $newFeedDate = $newFeed->getDateTime();
+
+                    $date1 = strtotime($addNewFeedDate->format('Y-m-d h:m:s'));
+                    $date2 = strtotime($newFeedDate->format('Y-m-d h:m:s'));
+
+                    if($date1 > $date2) {
+                        //get date diff
+                        $dateDiff = $date1 - $date2;
+                    }
+                    else{
+                        //get date diff
+                        $dateDiff = $date2 - $date1;
+                    }
+
+                    //get different by minutes
+                    $minutes = $dateDiff / 60;
 
                     //get high date
-                    $date = $addNewFeed->getDateTime() > $newFeed->getDateTime() ? $addNewFeed->getDateTime() : $newFeed->getDateTime();
-
-                    //get different days
-                    $minutes = $dateDiff->format("%i");
+                    $date = $addNewFeedDate > $newFeedDate ? $addNewFeedDate : $newFeedDate;
 
                     if($minutes <= 30) {
 
@@ -86,14 +98,26 @@ class ActivityGroupCommand extends ContainerAwareCommand
                         continue;
                     }
 
-                    //get date diff
-                    $dateDiff = date_diff($doneNewFeed->getDateTime(), $newFeed->getDateTime(), true);
+                    $doneNewFeedDate = $doneNewFeed->getDateTime();
+                    $newFeedDate = $newFeed->getDateTime();
+
+                    $date1 = strtotime($doneNewFeedDate->format('Y-m-d h:m:s'));
+                    $date2 = strtotime($newFeedDate->format('Y-m-d h:m:s'));
+
+                    if($date1 > $date2) {
+                        //get date diff
+                        $dateDiff = $date1 - $date2;
+                    }
+                    else{
+                        //get date diff
+                        $dateDiff = $date2 - $date1;
+                    }
+
+                    //get different by minutes
+                    $minutes = $dateDiff / 60;
 
                     //get high date
-                    $date = $doneNewFeed->getDateTime() > $newFeed->getDateTime() ? $doneNewFeed->getDateTime() : $newFeed->getDateTime();
-
-                    //get different days
-                    $minutes = $dateDiff->format("%i");
+                    $date = $doneNewFeedDate > $newFeedDate ? $doneNewFeedDate : $newFeedDate;
 
                     if($minutes <= 30) {
                         //get goal in new feed
@@ -113,14 +137,18 @@ class ActivityGroupCommand extends ContainerAwareCommand
                 }
             }
 
-            $em->flush();
-            $em->clear();
+            if ($counter > 50){
+
+                $em->flush();
+                $counter = 0;
+                $em->clear();
+            }
 
             $counter++;
             $progress->advance();
-
         }
 
+        $em->flush();
         $progress->finish();
         $output->writeln("<info>Success</info>");
     }
