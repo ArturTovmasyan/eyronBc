2726c2070a1e803aaa48ef83c53d74c37311157f
ARTUR <artur.tovmasyan777@gmail.com>
2016-08-22 15:48:44 +0400

    Create test for sitemap.xml

--- src/AppBundle/Tests/Controller/MainControllerTest.php
+++ src/AppBundle/Tests/Controller/MainControllerTest.php
@@ -7,6 +7,83 @@ use Symfony\Component\HttpFoundation\Response;
 class MainControllerTest extends BaseClass
 {
     /**
+     * This function is used to check sitemap
+     */
+    public function testSitemapXml()
+    {
+        // try to open page
+        $this->client->request('GET', '/sitemap.xml');
+
+        //get content in response
+        $content = $this->client->getResponse()->getContent();
+
+        // Assert that the response status code is 2xx
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), "can not open sitemap.xml");
+
+        //get xml data in content
+        $xml = simplexml_load_string($content);
+
+        //json_encode xml data
+        $xmlDataEncode = json_encode($xml);
+
+        //json_decode xml data for get json structure
+        $xmlData = json_decode($xmlDataEncode, true);
+
+        //get sitemap array
+        $siteMap = $xmlData['sitemap'];
+
+        $this->assertArrayHasKey('loc', $siteMap, 'Invalid loc key in sitemap.xml response');
+        $this->assertArrayHasKey('lastmod', $siteMap, 'Invalid lastmod key in sitemap.xml response');
+
+        //get sitemap location
+        $siteMapLoc = $siteMap['loc'];
+
+        //get array data for deafult sitemap.xml url
+        $xmlPath = parse_url($siteMapLoc);
+
+        //get default sitemap path
+        $defaultXmlPath = $xmlPath['path'];
+
+        // try to open page
+        $this->client->request('GET', $defaultXmlPath);
+
+        // Assert that the response status code is 2xx
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), "can not open sitemap.default.xml");
+
+        //get content in response
+        $defaultXmlData = $this->client->getResponse()->getContent();
+
+        //get xml data in content
+        $xml = simplexml_load_string($defaultXmlData);
+
+        //json_encode xml data
+        $defaultXmlDataEncode = json_encode($xml);
+
+        //json_decode xml data for get json structure
+        $xmlData = json_decode($defaultXmlDataEncode, true);
+
+        if(array_key_exists('url', $xmlData)) {
+
+            //get default sitemap data
+            $defaultXmlArrays = $xmlData['url'];
+
+            foreach ($defaultXmlArrays as $defaultXmlArray)
+            {
+                $this->assertArrayHasKey('loc', $defaultXmlArray, 'Invalid loc key in sitemap.default.xml response');
+                $this->assertArrayHasKey('lastmod', $defaultXmlArray, 'Invalid lastmod key in sitemap.default.xml response');
+                $this->assertArrayHasKey('changefreq', $defaultXmlArray, 'Invalid changefreq key in sitemap.default.xml response');
+                $this->assertArrayHasKey('priority', $defaultXmlArray, 'Invalid priority key in sitemap.default.xml response');
+            }
+        }
+
+        // Check that the profiler is enabled
+        if ($profile = $this->client->getProfile()){
+            // check the number of requests
+            $this->assertLessThan(10, $profile->getCollector('db')->getQueryCount(), "number of requests are much more greater than needed on page!");
+        }
+    }
+
+    /**
      * This function is used to check goal description content in web and meta tag
      */
     public function testDescriptionContent()
