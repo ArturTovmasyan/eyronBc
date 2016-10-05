355c81d48d08a367f44d5d57d2854c8bfa5bbd14
gevor <gsmgevor.gs@gmail.com>
2016-10-05 13:50:28 +0400

    fix responsive affiliate

--- src/AppBundle/Resources/public/js/angular/modules/affiliate/adds.js
+++ src/AppBundle/Resources/public/js/angular/modules/affiliate/adds.js
@@ -25,6 +25,14 @@ angular.module('adds', ['Interpolation',
           AffiliateDataManager.affiliate({},{page_link: scope.link, zone: scope.zone}, function (res) {
             if(res.length){
               angular.forEach(res, function (affiliate) {
+                if(window.innerWidth < 350){
+                  affiliate.html_content = affiliate.html_content.replace('data-width="370"', 'data-width="300"');
+                } else if(window.innerWidth < 580){
+                  affiliate.html_content = affiliate.html_content.replace('data-width="370"', 'data-width="'+ (window.innerWidth - 25) +'"');
+                } else if(window.innerWidth < 768){
+                  affiliate.html_content = affiliate.html_content.replace('data-width="370"', 'data-width="600"');
+                }
+
                 element.append(affiliate.html_content);
                 element.append(affiliate.js_content);
               });
