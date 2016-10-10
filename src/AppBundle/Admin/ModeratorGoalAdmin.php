14da33c999bb6197211d68e50aaef8b6b3d439d9
aram simonyan <asimonyan87@gmail.com>
2016-10-10 16:16:23 +0400

    add trait

--- src/AppBundle/Admin/ModeratorGoalAdmin.php
+++ src/AppBundle/Admin/ModeratorGoalAdmin.php
@@ -16,6 +16,7 @@ use AppBundle\Form\Type\BlMultipleFileType;
 use AppBundle\Form\Type\BlMultipleVideoType;
 use AppBundle\Form\Type\LocationType;
 use AppBundle\Model\PublishAware;
+use AppBundle\Traits\GoalAdminTrait;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -32,6 +33,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
  */
 class ModeratorGoalAdmin extends AbstractAdmin
 {
+    use GoalAdminTrait;
+
     protected $datagridValues = array(
         '_page' => 1,
         '_sort_order' => 'DESC',
@@ -163,16 +166,6 @@ class ModeratorGoalAdmin extends AbstractAdmin
     /**
      * {@inheritdoc}
      */
-    public function prePersist($object)
-    {
-        $object->setPublish(PublishAware::PUBLISH);
-        $object->setStatus(Goal::PUBLIC_PRIVACY);
-        $this->preUpdate($object);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function preUpdate($object)
     {
         $original = $this->getModelManager()->getEntityManager($this->getClass())->getUnitOfWork()->getOriginalEntityData($object);
@@ -184,173 +177,10 @@ class ModeratorGoalAdmin extends AbstractAdmin
             ;
         }
 
-        $user = $this->getConfigurationPool()->getContainer()->get('security.token_storage')->getToken()->getUser();
+        $this->updateData($object);
 
-        // check and set author
-        if((!$original && $object->getPublish() ==  PublishAware::PUBLISH) ||
-            ($original['publish'] != $object->getPublish() &&
-                $object->getPublish() == PublishAware::PUBLISH)){
-
-            $object->setPublishedBy($user->getUserName());
-        }
-
-        // get current user
-        $description = $object->getDescription();
-        $object->setDescription($description);
-
-        $object->setEditor($user);
-        $object->setReadinessStatus(Goal::TO_PUBLISH);
-
-        $this->getAndAddTags($object);
-        $this->addImages($object);
-
-        if ($videoLinks = $object->getVideoLink()){
-            $videoLinks = array_values($videoLinks);
-            $videoLinks = array_filter($videoLinks);
-
-            $object->setVideoLink($videoLinks);
-        }
     }
 
-    /**
-     * @param $object
-     */
-    private function getAndAddTags($object)
-    {
-        // get container
-        $container =  $this->getConfigurationPool()->getContainer();
-
-        // get entity manager
-        $em = $container->get('doctrine')->getManager();
-
-        // get content
-        $content = $object->getDescription();
-
-        // get tags from description
-        $tags = $this->getHashTags($content);
-
-        // check tags
-        if($tags){
-
-            // get tags from db
-            $dbTags = $em->getRepository("AppBundle:Tag")->getTagTitles();
-
-            // get new tags
-            $newTags = array_diff($tags, $dbTags);
-
-            // tags that is already exist in database
-            $existTags = array_diff($tags, $newTags);
-
-            // get tags from database
-            $oldTags = $em->getRepository("AppBundle:Tag")->findTagsByTitles($existTags);
-
-            // loop for array
-            foreach($newTags as $tagString){
-
-                // create new tag
-                $tag = new Tag();
-
-                $title = strtolower($tagString);
-
-                // replace ',' symbols
-                $title = str_replace(',', '', $title);
-
-                // replace ':' symbols
-                $title = str_replace(':', '', $title);
-
-                // replace '.' symbols
-                $title = str_replace('.', '', $title);
-
-                // set tag title
-                $tag->setTag($title);
-
-                // add tag
-                $object->addTag($tag);
-
-                // persist tag
-                $em->persist($tag);
-
-            }
-
-            // loop for tags n database
-            foreach($oldTags as $oldTag){
-
-                // check tag in collection
-                if(!$object->getTags() || !$object->getTags()->contains($oldTag)){
-
-                    // add tag
-                    $object->addTag($oldTag);
-
-                    // persist tag
-                    $em->persist($oldTag);
-                }
-            }
-
-            $em->flush();
-
-        }
-    }
-
-    /**
-     * @param $text
-     * @return mixed
-     */
-    private function getHashTags($text)
-    {
-        // get description
-        $content = strtolower($text);
-
-        // get hash tags
-        preg_match_all("/#(\w+)/", $content, $hashTags);
-
-        // return hash tags
-        return $hashTags[1];
-    }
-
-    /**
-     * @param $object
-     */
-    private function addImages($object)
-    {
-        $bucketService = $this->getConfigurationPool()->getContainer()->get('bl_service');
-
-        //get images
-        $images = $object->getImages();
-
-        // check images
-        if($images) {
-
-            $hasListPhoto = false;
-            $hasCoverPhoto = false;
-
-            // loop for images
-            foreach($images as $image) {
-                if (!($image instanceof GoalImage)){
-                    $object->removeImage($image);
-                    continue;
-                }
-
-                if ($image->getList() == true){
-                    $hasListPhoto = true;
-                }
-                if ($image->getCover() == true){
-                    $hasCoverPhoto = true;
-                }
-
-                // upload file
-                $bucketService->uploadFile($image);
-                $image->setGoal($object);
-            }
-
-            if (!$hasListPhoto && $images->first()){
-                $images->first()->setList(true);
-            }
-
-            if (!$hasCoverPhoto && $images->first()){
-                $images->first()->setCover(true);
-            }
-        }
-    }
 
     /**
      * @param string $context
--- src/AppBundle/Traits/GoalAdmin.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: aram
- * Date: 10/10/16
- * Time: 3:28 PM
- */
