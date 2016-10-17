8b59a5b157e9f0234d3984add8f2e662843e1b8f
aram simonyan <asimonyan87@gmail.com>
2016-10-17 13:47:10 +0400

    create badge service

---
+++ src/Application/UserBundle/Services/BadgeService.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aram
+ * Date: 10/17/16
+ * Time: 1:34 PM
+ */
+
+namespace Application\UserBundle\Services;
+
+/**
+ * Class BadgeService
+ * @package Application\UserBundle\Services
+ */
+class BadgeService
+{
+
+}
