2de5b3a34de2767c16e1c2b72002d68c0013075b
gevor <gsmgevor.gs@gmail.com>
2016-10-19 17:38:22 +0400

    fix leaderboard user name corection

--- src/AppBundle/Resources/public/js/angular/modules/leaderboard/leaderboard.js
+++ src/AppBundle/Resources/public/js/angular/modules/leaderboard/leaderboard.js
@@ -16,6 +16,7 @@ angular.module('leaderboard', ['Interpolation',
   function ($scope, envPrefix, LeaderboardConstant, $http) {
     $scope.users = [];
     $scope.usersCount = 10;
+    $scope.isMobile = (window.innerWidth < 768);
     $scope.LeaderboardConstant = LeaderboardConstant;
     $scope.pageStatus = LeaderboardConstant.TYPE_TRAVELLER;
     var url = envPrefix + 'api/v1.0/badges/{type}/topusers/' + $scope.usersCount;
@@ -60,6 +61,14 @@ angular.module('leaderboard', ['Interpolation',
         //   $(".modal-loading").hide();
         // }
       });
+    };
+
+    $scope.getFullName = function (user) {
+      if($scope.isMobile){
+        return (user.first_name.length > 16)?(user.first_name.substr(0,13) + '...'):(user.first_name.length + user.last_name.length > 16)?(user.first_name + ' ' + user.last_name.substr(0,16 - user.first_name.length) + '...'): user.first_name + ' ' + user.last_name;
+      } else {
+        return user.first_name + ' ' + user.last_name;
+      }
     }
   }
 ]);
