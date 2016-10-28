6d1cc6be981b55f6aed5ca08cd10322c9d6ccbe2
ARTUR <artur.tovmasyan777@gmail.com>
2016-10-28 17:16:18 +0400

    Fix amp template for blog

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -16,58 +16,12 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class BlogController extends Controller
 {
-//    const LIMIT = 10;
-
     /**
      * @Route("/blog", name="blog_list")
-     * @Cache(expires="+1 month", public=true)
      * @return Response
      */
     public function listAction()
     {
-//        //get entity manager
-//        $em = $this->getDoctrine()->getManager();
-//
-//        //get page number
-//        $page = $request->query->get('page');
-//
-//        //generate first number by page
-//        if($page > 1) {
-//            $first = ($page - 1) * self::LIMIT;
-//        }
-//        else {
-//            $first = 0;
-//        }
-//
-//        //get last updated date for caching
-//        $lastModifiedDate = $em->getRepository('AppBundle:Blog')->findLastUpdated($first, self::LIMIT);
-//
-//        //new response
-//        $response = new Response();
-//
-//        // set last modified data
-//        $response->setLastModified($lastModifiedDate);
-//
-//        // Set response as public. Otherwise it will be private by default.
-//        $response->setPublic();
-//
-//        // Check that the Response is not modified for the given Request
-//        if ($response->isNotModified($request)) {
-//            // return the 304 Response immediately
-//            return $response;
-//        }
-//
-//        //get all blog
-//        $blog = $em->getRepository('AppBundle:Blog')->findAllBlog();
-//
-//        //get paginator
-//        $paginator = $this->get('knp_paginator');
-//
-//        $pagination = $paginator->paginate(
-//            $blog,
-//            $request->query->getInt('page', 1)/*page number*/,
-//            self::LIMIT
-//        );
         return $this->render('AppBundle:Blog:list.html.twig');
     }
 
@@ -90,27 +44,27 @@ class BlogController extends Controller
             throw $this->createNotFoundException("Blog not found");
         }
 
-        //new response
-        $response = new Response();
-
-        // set last modified data
-        $response->setLastModified($blog->getUpdated());
-
-        // Set response as public. Otherwise it will be private by default.
-        $response->setPublic();
-
-        // Check that the Response is not modified for the given Request
-        if ($response->isNotModified($request)) {
-            // return the 304 Response immediately
-            return $response;
-        }
+//        //new response
+//        $response = new Response();
+//
+//        // set last modified data
+//        $response->setLastModified($blog->getUpdated());
+//
+//        // Set response as public. Otherwise it will be private by default.
+//        $response->setPublic();
+//
+//        // Check that the Response is not modified for the given Request
+//        if ($response->isNotModified($request)) {
+//            // return the 304 Response immediately
+//            return $response;
+//        }
 
         //add goals in arrayCollection
         $goalIds = $blog->getRelatedGoalIds();
         $relatedGoals = $em->getRepository('AppBundle:Goal')->findGoalByIds($goalIds);
         $blog->addGoals($relatedGoals);
 
-        return $this->render('AppBundle:Blog:show.html.twig', ['blog' => $blog], $response);
+        return $this->render('AppBundle:Blog:show.html.twig', ['blog' => $blog]);
     }
 }
 
