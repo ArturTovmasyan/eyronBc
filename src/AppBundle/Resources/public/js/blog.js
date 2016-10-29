2e7a8c273c3629e5e9dfc2359387e1b46137f5f1
gevor <gsmgevor.gs@gmail.com>
2016-10-29 17:44:00 +0400

    fix edit by js

--- src/AppBundle/Resources/public/js/blog.js
+++ src/AppBundle/Resources/public/js/blog.js
@@ -18,6 +18,12 @@ jQuery(document).ready(function() {
     // index when inserting a new item (e.g. 2)
     $collectionHolder.data('index', $collectionHolder.find(':input').length);
 
+    for(var index = 0; index < $collectionHolder.data('index'); index++){
+        if($("div[id$='_" + index+"_type']").length){
+            simpleTypeByIndex(index, 'div[id$="_bl_multiple_blog_'+index+'"');
+        }
+    }
+
     $addTagLink.on('click', function(e) {
         // prevent the link from creating a "#" on the URL
         e.preventDefault();
@@ -45,35 +51,7 @@ function addTagForm($collectionHolder, $newLinkLi) {
     var $newFormLi = $('<li class="file-list col-sm-3"></li>').append(newForm);
     addTagFormDeleteLink($newFormLi);
     $newLinkLi.after($newFormLi);
-    select2TegIds[index] = '#' + $(newForm).find( "select" ).last()[0].id;
-    $(select2TegIds[index]).parent().parent().hide();
-
-    $("select[id$='_" + index+"_type']").change(function(ev){
-        var choice = ev.target.value;
-        if(choice == 'goal'){
-            $(select2TegIds[index]).parent().parent().show();
-            $(select2TegIds[index]).select2();
-            $("div[id$='_" + index+"_content']").hide()
-        } else if(choice == 'text'){
-            $("div[id$='_" + index+"_content']").show();
-            $(select2TegIds[index]).parent().parent().hide();
-        }
-    });
-
-    $("select[id$='_" + index+"_goal']").change(function(ev){
-        var id = ev.val;
-        $.get( "/api/v1.0/goals/image/"+id, function( data ) {
-            var img = $("img[id='_" + index+"_goal']");
-            if(img.length){
-                img.attr("src",data.image_path);
-            } else {
-                img = $('<img style="width: 270px;height: 200px;margin-bottom: 13px;" src="' + data.image_path + '" alt="goal image" id="_'+index+'_goal">');
-                img.prependTo($("div[id$='_" + index+"_goal']").first())
-            }
-
-        });
-
-    });
+    simpleTypeByIndex(index, newForm);
 }
 
 function addTagFormDeleteLink($newFormLi)
@@ -87,6 +65,61 @@ function addTagFormDeleteLink($newFormLi)
 
         // remove the li for the tag form
         $newFormLi.remove();
+    });
+}
+
+function simpleTypeByIndex(index, newForm) {
+    select2TegIds[index] = '#' + $(newForm).find( "select" ).last()[0].id;
+    var type = $("select[id$='_" + index+"_type']").val();
+    if(type == 'goal'){
+        $("textarea[id$='_" + index+"_content']").parent().parent().hide();
+        var goalId = $("select[id$='_" + index+"_goal']").val();
+
+        $.get( "/api/v1.0/goals/image/"+goalId, function( data ) {
+            var img = $("img[id='_" + index+"_goal']");
+            if(img.length){
+                img.attr("src",data.image_path);
+            } else {
+                img = $('<img style="width: 270px;height: 200px;margin-bottom: 13px;" src="' + data.image_path + '" alt="goal image" id="_'+index+'_goal">');
+                img.prependTo($("div[id$='_" + index+"_goal']").first())
+            }
+
+        });
+    } else {
+        $(select2TegIds[index]).parent().parent().hide();
+    }
+
+    $("select[id$='_" + index+"_type']").change(function(ev){
+        var choice = ev.target.value;
+        if(choice == 'goal'){
+            $(select2TegIds[index]).parent().parent().show();
+            $(select2TegIds[index]).select2();
+            var goalId = $("select[id$='_" + index+"_goal']").val();
+            if(goalId){
+                $("textarea[id$='_" + index+"_content']").val(goalId);
+            }
+            $("div[id$='_" + index+"_content']").hide()
+        } else if(choice == 'text'){
+            $("textarea[id$='_" + index+"_content']").val('');
+            $("div[id$='_" + index+"_content']").show();
+            $(select2TegIds[index]).parent().parent().hide();
+        }
+    });
+
+    $("select[id$='_" + index+"_goal']").change(function(ev){
+        var id = ev.val;
+        $("textarea[id$='_" + index+"_content']").val(id);
+        $.get( "/api/v1.0/goals/image/"+id, function( data ) {
+            var img = $("img[id='_" + index+"_goal']");
+            if(img.length){
+                img.attr("src",data.image_path);
+            } else {
+                img = $('<img style="width: 270px;height: 200px;margin-bottom: 13px;" src="' + data.image_path + '" alt="goal image" id="_'+index+'_goal">');
+                img.prependTo($("div[id$='_" + index+"_goal']").first())
+            }
+
+        });
+
     });
 }
 
