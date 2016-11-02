1d134bba7a163cbd5a9ebd79245848f8c3f4b353
aram simonyan <asimonyan87@gmail.com>
2016-11-02 17:35:49 +0400

    fix migration issue

--- app/DoctrineMigrations/Version20161025095829.php
+++ app/DoctrineMigrations/Version20161025095829.php
@@ -26,6 +26,7 @@ class Version20161025095829 extends AbstractMigration
         $this->addSql('UPDATE goal_image set list_image = 0 WHERE list_image IS NULL');
         $this->addSql('UPDATE goal_image set cover_image = 0 WHERE cover_image IS NULL');
         $this->addSql('ALTER TABLE goal_image CHANGE list_image list_image TINYINT(1) NOT NULL, CHANGE cover_image cover_image TINYINT(1) NOT NULL');
+        $this->addSql('UPDATE fos_user set created_at = "2016:01:01 01:01" WHERE created_at is NULL ');
 
     }
 
