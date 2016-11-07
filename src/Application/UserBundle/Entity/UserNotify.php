708da410a71d884aa2a9a6340de9c0e28bff1065
aram simonyan <asimonyan87@gmail.com>
2016-11-07 11:15:09 +0400

    user notify entity

--- src/Application/UserBundle/Entity/UserNotify.php
+++ src/Application/UserBundle/Entity/UserNotify.php
@@ -1,70 +1,653 @@
 <?php
-//namespace Application\UserBundle\Entity;
-//
-//use Doctrine\ORM\Mapping as ORM;
-//use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-//use Symfony\Component\Validator\Constraints as Assert;
-//use Gedmo\Mapping\Annotation as Gedmo;
-//use JMS\Serializer\Annotation\Groups;
-//
-///**
-// * @ORM\Entity()
-// * @ORM\Table(name="user_notify")
-// */
-//class UserNotify
-//{
-//    /**
-//     * @ORM\Id
-//     * @ORM\Column(type="integer")
-//     * @ORM\GeneratedValue(strategy="AUTO")
-//     */
-//    protected $id;
-//
-//    /**
-//     * @ORM\Column(name="is_comment_notify", type="boolean", nullable=true)
-//     * @var
-//     * @Groups({"settings"})
-//     */
-//    private $isCommentNotify = true;
-//
-//    /**
-//     * @ORM\Column(name="is_success_story_notify", type="boolean", nullable=true)
-//     * @var
-//     * @Groups({"settings"})
-//     */
-//    private $isSuccessStoryNotify = true;
-//
-//    /**
-//     * @ORM\Column(name="is_comment_push_note", type="boolean", nullable=true)
-//     * @var
-//     * @Groups({"settings"})
-//     */
-//    private $isCommentPushNote = true;
-//
-//    /**
-//     * @ORM\Column(name="is_success_story_push_note", type="boolean", nullable=true)
-//     * @var
-//     * @Groups({"settings"})
-//     */
-//    private $isSuccessStoryPushNote = true;
-//
-//    /**
-//     * @ORM\Column(name="is_progress_push_note", type="boolean", nullable=true)
-//     * @var
-//     * @Groups({"settings"})
-//     */
-//    private $isProgressPushNote = true;
-//
-//}
-//
-////
-////Goals commented
-////Ideas commented
-////Success stories for Goals
-////Success stories for Ideas
-////Success stories liked
-////Published Ideas
-////Comments replied
-////Deadline expires
-////New Goalfriends
-////New Ideas                 
+namespace Application\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use Gedmo\Mapping\Annotation as Gedmo;
+use JMS\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="user_notify")
+ */
+class UserNotify
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="is_comment_on_goal_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isCommentOnGoalNotify = true;
+
+    /**
+     * @ORM\Column(name="is_comment_on_idea_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isCommentOnIdeaNotify = true;
+
+    /**
+     * @ORM\Column(name="is_comment_on_goal_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isCommentOnGoalPush = true;
+
+    /**
+     * @ORM\Column(name="is_comment_on_idea_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isCommentOnIdeaPush = true;
+
+    /**
+     * @ORM\Column(name="is_success_story_on_goal_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isSuccessStoryOnGoalNotify = true;
+
+    /**
+     * @ORM\Column(name="is_success_story_on_idea_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isSuccessStoryOnIdeaNotify = true;
+
+    /**
+     * @ORM\Column(name="is_success_story_on_goal_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isSuccessStoryOnGoalPush = true;
+
+    /**
+     * @ORM\Column(name="is_success_story_on_idea_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isSuccessStoryOnIdeaPush = true;
+
+
+    /**
+     * @ORM\Column(name="is_success_story_like_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isSuccessStoryLikeNotify = true;
+
+    /**
+     * @ORM\Column(name="is_success_story_like_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isSuccessStoryLikePush = true;
+
+    /**
+     * @ORM\Column(name="is_goal_publish_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isGoalPublishNotify = true;
+
+    /**
+     * @ORM\Column(name="is_goal_publish_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isGoalPublishPush = true;
+
+    /**
+     * @ORM\Column(name="is_comment_reply_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isCommentReplyNotify = true;
+
+    /**
+     * @ORM\Column(name="is_comment_replyh_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isCommentReplyhPush = true;
+
+    /**
+     * @ORM\Column(name="is_deadline_exp_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isDeadlineExpNotify = true;
+
+    /**
+     * @ORM\Column(name="is_deadline_exp_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isDeadlineExpPush = true;
+
+    /**
+     * @ORM\Column(name="is_new_goal_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isNewGoalNotify = true;
+
+    /**
+     * @ORM\Column(name="is_new_goal_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isNewGoalPush = true;
+
+    /**
+     * @ORM\Column(name="is_new_idea_notify", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isNewIdeaNotify = true;
+
+    /**
+     * @ORM\Column(name="is_new_idea_push", type="boolean", nullable=true)
+     * @var
+     * @Groups({"settings"})
+     */
+    private $isNewIdeaPush = true;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set isCommentOnGoalNotify
+     *
+     * @param boolean $isCommentOnGoalNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsCommentOnGoalNotify($isCommentOnGoalNotify)
+    {
+        $this->isCommentOnGoalNotify = $isCommentOnGoalNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isCommentOnGoalNotify
+     *
+     * @return boolean
+     */
+    public function getIsCommentOnGoalNotify()
+    {
+        return $this->isCommentOnGoalNotify;
+    }
+
+    /**
+     * Set isCommentOnIdeaNotify
+     *
+     * @param boolean $isCommentOnIdeaNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsCommentOnIdeaNotify($isCommentOnIdeaNotify)
+    {
+        $this->isCommentOnIdeaNotify = $isCommentOnIdeaNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isCommentOnIdeaNotify
+     *
+     * @return boolean
+     */
+    public function getIsCommentOnIdeaNotify()
+    {
+        return $this->isCommentOnIdeaNotify;
+    }
+
+    /**
+     * Set isCommentOnGoalPush
+     *
+     * @param boolean $isCommentOnGoalPush
+     *
+     * @return UserNotify
+     */
+    public function setIsCommentOnGoalPush($isCommentOnGoalPush)
+    {
+        $this->isCommentOnGoalPush = $isCommentOnGoalPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isCommentOnGoalPush
+     *
+     * @return boolean
+     */
+    public function getIsCommentOnGoalPush()
+    {
+        return $this->isCommentOnGoalPush;
+    }
+
+    /**
+     * Set isCommentOnIdeaPush
+     *
+     * @param boolean $isCommentOnIdeaPush
+     *
+     * @return UserNotify
+     */
+    public function setIsCommentOnIdeaPush($isCommentOnIdeaPush)
+    {
+        $this->isCommentOnIdeaPush = $isCommentOnIdeaPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isCommentOnIdeaPush
+     *
+     * @return boolean
+     */
+    public function getIsCommentOnIdeaPush()
+    {
+        return $this->isCommentOnIdeaPush;
+    }
+
+    /**
+     * Set isSuccessStoryOnGoalNotify
+     *
+     * @param boolean $isSuccessStoryOnGoalNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsSuccessStoryOnGoalNotify($isSuccessStoryOnGoalNotify)
+    {
+        $this->isSuccessStoryOnGoalNotify = $isSuccessStoryOnGoalNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isSuccessStoryOnGoalNotify
+     *
+     * @return boolean
+     */
+    public function getIsSuccessStoryOnGoalNotify()
+    {
+        return $this->isSuccessStoryOnGoalNotify;
+    }
+
+    /**
+     * Set isSuccessStoryOnIdeaNotify
+     *
+     * @param boolean $isSuccessStoryOnIdeaNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsSuccessStoryOnIdeaNotify($isSuccessStoryOnIdeaNotify)
+    {
+        $this->isSuccessStoryOnIdeaNotify = $isSuccessStoryOnIdeaNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isSuccessStoryOnIdeaNotify
+     *
+     * @return boolean
+     */
+    public function getIsSuccessStoryOnIdeaNotify()
+    {
+        return $this->isSuccessStoryOnIdeaNotify;
+    }
+
+    /**
+     * Set isSuccessStoryOnGoalPush
+     *
+     * @param boolean $isSuccessStoryOnGoalPush
+     *
+     * @return UserNotify
+     */
+    public function setIsSuccessStoryOnGoalPush($isSuccessStoryOnGoalPush)
+    {
+        $this->isSuccessStoryOnGoalPush = $isSuccessStoryOnGoalPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isSuccessStoryOnGoalPush
+     *
+     * @return boolean
+     */
+    public function getIsSuccessStoryOnGoalPush()
+    {
+        return $this->isSuccessStoryOnGoalPush;
+    }
+
+    /**
+     * Set isSuccessStoryOnIdeaPush
+     *
+     * @param boolean $isSuccessStoryOnIdeaPush
+     *
+     * @return UserNotify
+     */
+    public function setIsSuccessStoryOnIdeaPush($isSuccessStoryOnIdeaPush)
+    {
+        $this->isSuccessStoryOnIdeaPush = $isSuccessStoryOnIdeaPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isSuccessStoryOnIdeaPush
+     *
+     * @return boolean
+     */
+    public function getIsSuccessStoryOnIdeaPush()
+    {
+        return $this->isSuccessStoryOnIdeaPush;
+    }
+
+    /**
+     * Set isSuccessStoryLikeNotify
+     *
+     * @param boolean $isSuccessStoryLikeNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsSuccessStoryLikeNotify($isSuccessStoryLikeNotify)
+    {
+        $this->isSuccessStoryLikeNotify = $isSuccessStoryLikeNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isSuccessStoryLikeNotify
+     *
+     * @return boolean
+     */
+    public function getIsSuccessStoryLikeNotify()
+    {
+        return $this->isSuccessStoryLikeNotify;
+    }
+
+    /**
+     * Set isSuccessStoryLikePush
+     *
+     * @param boolean $isSuccessStoryLikePush
+     *
+     * @return UserNotify
+     */
+    public function setIsSuccessStoryLikePush($isSuccessStoryLikePush)
+    {
+        $this->isSuccessStoryLikePush = $isSuccessStoryLikePush;
+
+        return $this;
+    }
+
+    /**
+     * Get isSuccessStoryLikePush
+     *
+     * @return boolean
+     */
+    public function getIsSuccessStoryLikePush()
+    {
+        return $this->isSuccessStoryLikePush;
+    }
+
+    /**
+     * Set isGoalPublishNotify
+     *
+     * @param boolean $isGoalPublishNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsGoalPublishNotify($isGoalPublishNotify)
+    {
+        $this->isGoalPublishNotify = $isGoalPublishNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isGoalPublishNotify
+     *
+     * @return boolean
+     */
+    public function getIsGoalPublishNotify()
+    {
+        return $this->isGoalPublishNotify;
+    }
+
+    /**
+     * Set isGoalPublishPush
+     *
+     * @param boolean $isGoalPublishPush
+     *
+     * @return UserNotify
+     */
+    public function setIsGoalPublishPush($isGoalPublishPush)
+    {
+        $this->isGoalPublishPush = $isGoalPublishPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isGoalPublishPush
+     *
+     * @return boolean
+     */
+    public function getIsGoalPublishPush()
+    {
+        return $this->isGoalPublishPush;
+    }
+
+    /**
+     * Set isCommentReplyNotify
+     *
+     * @param boolean $isCommentReplyNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsCommentReplyNotify($isCommentReplyNotify)
+    {
+        $this->isCommentReplyNotify = $isCommentReplyNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isCommentReplyNotify
+     *
+     * @return boolean
+     */
+    public function getIsCommentReplyNotify()
+    {
+        return $this->isCommentReplyNotify;
+    }
+
+    /**
+     * Set isCommentReplyhPush
+     *
+     * @param boolean $isCommentReplyhPush
+     *
+     * @return UserNotify
+     */
+    public function setIsCommentReplyhPush($isCommentReplyhPush)
+    {
+        $this->isCommentReplyhPush = $isCommentReplyhPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isCommentReplyhPush
+     *
+     * @return boolean
+     */
+    public function getIsCommentReplyhPush()
+    {
+        return $this->isCommentReplyhPush;
+    }
+
+    /**
+     * Set isDeadlineExpNotify
+     *
+     * @param boolean $isDeadlineExpNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsDeadlineExpNotify($isDeadlineExpNotify)
+    {
+        $this->isDeadlineExpNotify = $isDeadlineExpNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isDeadlineExpNotify
+     *
+     * @return boolean
+     */
+    public function getIsDeadlineExpNotify()
+    {
+        return $this->isDeadlineExpNotify;
+    }
+
+    /**
+     * Set isDeadlineExpPush
+     *
+     * @param boolean $isDeadlineExpPush
+     *
+     * @return UserNotify
+     */
+    public function setIsDeadlineExpPush($isDeadlineExpPush)
+    {
+        $this->isDeadlineExpPush = $isDeadlineExpPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isDeadlineExpPush
+     *
+     * @return boolean
+     */
+    public function getIsDeadlineExpPush()
+    {
+        return $this->isDeadlineExpPush;
+    }
+
+    /**
+     * Set isNewGoalNotify
+     *
+     * @param boolean $isNewGoalNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsNewGoalNotify($isNewGoalNotify)
+    {
+        $this->isNewGoalNotify = $isNewGoalNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isNewGoalNotify
+     *
+     * @return boolean
+     */
+    public function getIsNewGoalNotify()
+    {
+        return $this->isNewGoalNotify;
+    }
+
+    /**
+     * Set isNewGoalPush
+     *
+     * @param boolean $isNewGoalPush
+     *
+     * @return UserNotify
+     */
+    public function setIsNewGoalPush($isNewGoalPush)
+    {
+        $this->isNewGoalPush = $isNewGoalPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isNewGoalPush
+     *
+     * @return boolean
+     */
+    public function getIsNewGoalPush()
+    {
+        return $this->isNewGoalPush;
+    }
+
+    /**
+     * Set isNewIdeaNotify
+     *
+     * @param boolean $isNewIdeaNotify
+     *
+     * @return UserNotify
+     */
+    public function setIsNewIdeaNotify($isNewIdeaNotify)
+    {
+        $this->isNewIdeaNotify = $isNewIdeaNotify;
+
+        return $this;
+    }
+
+    /**
+     * Get isNewIdeaNotify
+     *
+     * @return boolean
+     */
+    public function getIsNewIdeaNotify()
+    {
+        return $this->isNewIdeaNotify;
+    }
+
+    /**
+     * Set isNewIdeaPush
+     *
+     * @param boolean $isNewIdeaPush
+     *
+     * @return UserNotify
+     */
+    public function setIsNewIdeaPush($isNewIdeaPush)
+    {
+        $this->isNewIdeaPush = $isNewIdeaPush;
+
+        return $this;
+    }
+
+    /**
+     * Get isNewIdeaPush
+     *
+     * @return boolean
+     */
+    public function getIsNewIdeaPush()
+    {
+        return $this->isNewIdeaPush;
+    }
+}
