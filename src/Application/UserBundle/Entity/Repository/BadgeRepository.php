c51ef30d63ee0dc6c0d4a012730c48e98d305314
Andranik <andranik.hovhan@gmail.com>
2016-11-11 11:04:23 +0400

    badge rest

--- src/Application/UserBundle/Entity/Repository/BadgeRepository.php
+++ src/Application/UserBundle/Entity/Repository/BadgeRepository.php
@@ -16,20 +16,65 @@ class BadgeRepository extends EntityRepository
      * 
      * @param $type
      * @param $count
+     * @param $userId
      * @return mixed
      * @throws \Doctrine\ORM\NonUniqueResultException
      */
-    public function findTopUsersByType($type, $count)
+    public function findTopUsersByType($type, $count, $userId = null)
     {
-        return $this->getEntityManager()
+        $topBadges = $this->getEntityManager()
             ->createQuery("SELECT b, u
                            FROM ApplicationUserBundle:Badge b
                            JOIN b.user u
                            WHERE b.type = :types
-                           ORDER BY b.score DESC")
+                           ORDER BY b.score DESC, u.id ASC")
             ->setParameter('types', $type)
             ->setMaxResults($count)
             ->getResult();
+
+        if (is_null($userId)){
+            return $topBadges;
+        }
+
+        $userPosition =  $this->getEntityManager()
+            ->createQuery("SELECT COUNT(b.id) as cnt
+                           FROM ApplicationUserBundle:Badge b
+                           JOIN b.user u
+                           WHERE b.type = :types 
+                           AND (b.score > (SELECT MAX(b1.score) FROM ApplicationUserBundle:Badge b1 WHERE b1.user = :user)                           
+                           OR (b.score = (SELECT MAX(b2.score) FROM ApplicationUserBundle:Badge b2 WHERE b2.user = :user) AND u.id < :user))")
+            ->setParameter('types', $type)
+            ->setParameter('user', $userId)
+            ->setMaxResults($count)
+            ->getSingleScalarResult();
+
+
+        if ($userPosition < 10){
+            return $topBadges;
+        }
+
+        $nearBadges = $this->getEntityManager()
+            ->createQuery("SELECT b, u
+                           FROM ApplicationUserBundle:Badge b
+                           JOIN b.user u
+                           WHERE b.type = :types
+                           ORDER BY b.score DESC, u.id ASC")
+            ->setParameter('types', $type)
+            ->setFirstResult($userPosition - 2 <= 10 ? 10 : $userPosition - 2)
+            ->setMaxResults($userPosition - 2 <= 10 ? $userPosition - 7 : 5)
+            ->getResult();
+
+        foreach($topBadges as $key => $topBadge){
+            $topBadge->position = $key;
+        }
+
+        $startPosition = $userPosition - 2 <= 10 ? 10 : $userPosition - 2;
+        foreach($nearBadges as $nearBadge){
+            $nearBadge->position = $startPosition;
+            $startPosition++;
+        }
+
+        return array_merge($topBadges, $nearBadges);
     }
 
     /**
