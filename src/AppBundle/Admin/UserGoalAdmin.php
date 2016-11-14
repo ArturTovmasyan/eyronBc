654928d9c680240276650816c7fca9eaeb5d58ed
ARTUR <artur.tovmasyan777@gmail.com>
2016-11-14 17:43:32 +0400

    Create all admin part

--- src/AppBundle/Admin/UserGoalAdmin.php
+++ src/AppBundle/Admin/UserGoalAdmin.php
@@ -6,9 +6,18 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
 
 class UserGoalAdmin extends AbstractAdmin
 {
+    /**
+     * @param RouteCollection $collection
+     */
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('create');
+    }
+
     /**
      * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
      *
@@ -18,16 +27,15 @@ class UserGoalAdmin extends AbstractAdmin
     {
         $showMapper
             ->add('id', null, ['label'=>'admin.label.name.id'])
-            ->add('status', null, ['label'=>'admin.label.name.status'])
+            ->add('getStringStatus', null, ['label'=>'admin.label.name.status'])
             ->add('dateStatus', null, ['label'=>'admin.label.name.date_status'])
             ->add('doDateStatus', null, ['label'=>'admin.label.name.do_date_status'])
             ->add('isVisible', null, ['label'=>'admin.label.name.is_visible'])
             ->add('urgent', null, ['label'=>'admin.label.name.urgent'])
             ->add('important', null, ['label'=>'admin.label.name.important'])
             ->add('note', null, ['label'=>'admin.label.name.note'])
-            ->add('steps', null, ['label'=>'goal.steps'])
             ->add('confirmed', null, ['label'=>'admin.label.name.confirmed'])
-            ->add('goal.id', null, ['label'=>'admin.label.name.goal'])
+            ->add('goal', null, ['label'=>'admin.label.name.goal', 'admin_code' => 'sonata.admin.app.goal'])
             ->add('user', null, ['label'=>'admin.label.name.user'])
             ->add('doDate', null, ['label'=>'admin.label.name.do_date'])
             ->add('completionDate', null, ['label'=>'admin.label.name.completion_date'])
@@ -40,20 +48,25 @@ class UserGoalAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('status', null, ['label'=>'admin.label.name.status'])
-            ->add('dateStatus', null, ['label'=>'admin.label.name.date_status'])
-            ->add('doDateStatus', null, ['label'=>'admin.label.name.do_date_status'])
+            ->add('status', 'choice', ['label'=>'admin.label.name.status',
+                            'choices'=>[1=>'user_goal.active', 2=>'user_goal.completed']])
+            ->add('dateStatus', 'choice', ['label'=>'admin.label.name.date_status',
+                                 'choices'=>[1=>'admin.label.name.all', 2=>'admin.label.name.only_year',
+                                             3=>'admin.label.name.only_year_month']])
+            ->add('doDateStatus','choice', ['label'=>'admin.label.name.do_date_status',
+                                 'choices'=>[1=>'admin.label.name.all', 2=>'admin.label.name.only_year',
+                                             3=>'admin.label.name.only_year_month']])
             ->add('isVisible', null, ['label'=>'admin.label.name.is_visible'])
             ->add('urgent', null, ['label'=>'admin.label.name.urgent'])
             ->add('important', null, ['label'=>'admin.label.name.important'])
             ->add('note', null, ['label'=>'admin.label.name.note', 'required' => false])
-            ->add('steps', null, ['label'=>'goal.steps'])
             ->add('confirmed', null, ['label'=>'admin.label.name.confirmed', 'data' => false, 'required' => false])
-//            ->add('goal.id', null, ['label'=>'admin.label.name.goal'])
-            ->add('user', null, ['label'=>'admin.label.name.user'])
-//            ->add('user', 'sonata_type_model_autocomplete', ['label'=>'admin.label.name.user', 'property'=>'username', 'multiple' => true, 'required' => false])
+            ->add('goal', 'sonata_type_model_autocomplete', ['label'=>'admin.label.name.goal', 'property' => 'title'], ['admin_code' => 'sonata.admin.app.goal'])
+            ->add('user', 'sonata_type_model_autocomplete', ['label'=>'admin.label.name.user', 'property' => 'email'])
             ->add('doDate', 'sonata_type_date_picker', ['label'=>'admin.label.name.do_date', 'required' => false])
             ->add('completionDate', 'sonata_type_date_picker', ['label'=>'admin.label.name.completion_date', 'required' => false])
+            ->add('listedDate', 'sonata_type_date_picker', ['label'=>'admin.label.name.listed_date'])
+            ->add('updated', 'sonata_type_date_picker', ['label'=>'admin.label.name.updated'])
         ;
     }
 
@@ -70,18 +83,16 @@ class UserGoalAdmin extends AbstractAdmin
             ->add('important', null, ['label'=>'admin.label.name.important'])
             ->add('confirmed', null, ['label'=>'admin.label.name.confirmed'])
             ->add('goal.title', null, ['label'=>'admin.label.name.goal_title'])
-
             ->add('user.email', null, ['label'=>'admin.label.name.email'])
             ->add('user.firstname', null, ['label'=>'admin.label.name.firstName'])
             ->add('user.lastname', null, ['label'=>'admin.label.name.lastName'])
-
             ->add('doDate', 'doctrine_orm_date_range', ['label' => 'admin.label.name.do_date'], 'sonata_type_date_range_picker',
                 [
                     'field_options_start'=> ['format'=>'yyyy-MM-dd'],
                     'field_options_end'=> ['format'=>'yyyy-MM-dd']
                 ]
             )
-            ->add('listedDate', 'doctrine_orm_date_range', ['label' => 'admin.label.name.created', 'show_filter' => true], 'sonata_type_date_range_picker',
+            ->add('listedDate', 'doctrine_orm_date_range', ['label' => 'admin.label.name.listed_date', 'show_filter' => true], 'sonata_type_date_range_picker',
                 [
                     'field_options_start'=> ['format'=>'yyyy-MM-dd'],
                     'field_options_end'=> ['format'=>'yyyy-MM-dd']
@@ -100,7 +111,7 @@ class UserGoalAdmin extends AbstractAdmin
             ->add('confirmed', null, ['label'=>'admin.label.name.confirmed'])
             ->add('urgent', null, ['label'=>'admin.label.name.urgent'])
             ->add('important', null, ['label'=>'admin.label.name.important'])
-            ->add('goal.id', null, ['label'=>'admin.label.name.goal'])
+            ->add('goal', null, ['label'=>'admin.label.name.goal', 'admin_code' => 'sonata.admin.app.goal'])
             ->add('user', null, ['label'=>'admin.label.name.user'])
             ->add('doDate', null, ['label'=>'admin.label.name.do_date'])
             ->add('completionDate', null, ['label'=>'admin.label.name.completion_date'])
