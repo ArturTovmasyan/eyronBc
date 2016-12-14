e037abcf9f3fa7e2cd05b6bc26cd19d4c4efd970
ARTUR <artur.tovmasyan777@gmail.com>
2016-12-14 13:36:57 +0400

    add my-list block

--- bucketlist/src/app/ideas/ideas.module.ts
+++ bucketlist/src/app/ideas/ideas.module.ts
@@ -1,26 +1,22 @@
 import { NgModule }       from '@angular/core';
 import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
-import {TranslateModule} from 'ng2-translate';
 
 import { IdeasComponent }    from './ideas.component';
 import { IdeasCategoryComponent }  from '../ideas-category/ideas-category.component';
 
 import { IdeasRouting } from './ideas-routing';
 
-import { MyListComponent } from '../block/my-list/my-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IdeasRouting,
-    TranslateModule.forRoot()
   ],
   declarations: [
     IdeasComponent,
     IdeasCategoryComponent,
-    MyListComponent
   ],
   providers: [
     // HeroService
