59d7ad4ecd9a669983c4992ac341cc09b4c6dc5b
ando94-23 <ando94-23@mail.ru>
2016-12-16 19:32:22 +0400

    creating complete profile block

---
+++ bucketlist/src/app/block/complete-profile/complete-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'complete-profile-block',
+  templateUrl: './complete-profile.component.html',
+  styleUrls: ['./complete-profile.component.less']
+})
+export class CompleteProfileBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
