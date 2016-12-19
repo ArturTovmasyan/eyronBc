c2e33f940f878678eed71c301980e725a4fd4b07
Ani <animanukyan23@gmail.com>
2016-12-19 17:42:37 +0400

    styles

--- bucketlist/src/app/components/goal/goal.component.ts
+++ bucketlist/src/app/components/goal/goal.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 import { Goal } from '../../interface/goal';
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.less']
+  styleUrls: ['./goal.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GoalComponent implements OnInit {
   @Input() goal: Goal;
