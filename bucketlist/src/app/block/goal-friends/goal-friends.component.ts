6dd9941668996e9538a2aef62845677cf9b66515
gevor <gsmgevor.gs@gmail.com>
2016-12-19 18:13:55 +0400

    fix img optimization

--- bucketlist/src/app/block/goal-friends/goal-friends.component.ts
+++ bucketlist/src/app/block/goal-friends/goal-friends.component.ts
@@ -29,6 +29,7 @@ export class GoalFriendsBlockComponent implements OnInit {
     if(data){
       this.users = data[1];
       this.length = data.length;
+      this.goalReserve();
     } else {
       this.goalFriends()
     }
@@ -51,12 +52,24 @@ export class GoalFriendsBlockComponent implements OnInit {
         .subscribe(
             data => {
               this.reserve = data;
+              for(let item of data[1]){
+                let img;
+                if(item.cached_image){
+                  img = new Image();
+                  img.src = item.cached_image;
+                }
+              }
               this._cacheService.set('goalFriendBox', this.reserve, {maxAge: 2 * 24 * 60 * 60});
             },
             error => this.errorMessage = <any>error);
   }
 
   refreshGoalFriends(){
+    // angular.element('#popularLoad').css({
+    //   '-webkit-transform': 'rotate('+deg+'deg)',
+    //   '-ms-transform': 'rotate('+deg+'deg)',
+    //   'transform': 'rotate('+deg+'deg)'
+    // });
     this.users = this.reserve[1];
     this.length = this.reserve.length;
     this.goalReserve()
