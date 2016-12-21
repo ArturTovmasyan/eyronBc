27cc59c28467e8bb766232100dba8937ad54a2e6
Ani <animanukyan23@gmail.com>
2016-12-21 12:54:16 +0400

    complete profile in activity page

--- bucketlist/src/app/block/complete-profile/complete-profile.component.ts
+++ bucketlist/src/app/block/complete-profile/complete-profile.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'complete-profile-block',
   templateUrl: './complete-profile.component.html',
-  styleUrls: ['./complete-profile.component.less']
+  styleUrls: ['./complete-profile.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CompleteProfileBlockComponent implements OnInit {
 
--- bucketlist/src/app/interface/goal.ts
+++ bucketlist/src/app/interface/goal.ts
@@ -2,6 +2,6 @@ export interface Goal {
     id: number,
     title: string,
     slug: string,
-    cachedImage: string,
+    cachedImage?: string,
     cached_image?: string
 }
