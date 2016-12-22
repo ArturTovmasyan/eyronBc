e35a674242f4d69af8a7d60388d890dd6c6a2ff7
gevor <gsmgevor.gs@gmail.com>
2016-12-22 19:41:07 +0400

    fix building project

--- web/dist/7.chunk.js
+++ web/dist/7.chunk.js
@@ -1,14 +1,14 @@
 webpackJsonp([7,13],{
 
-/***/ 772:
+/***/ 793:
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__(80);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__drafts_component__ = __webpack_require__(782);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__draft_routing__ = __webpack_require__(800);
-/* harmony export (binding) */ __webpack_require__.d(exports, "DraftsModule", function() { return DraftsModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__goal_create_component__ = __webpack_require__(829);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__goal_create_routing__ = __webpack_require__(849);
+/* harmony export (binding) */ __webpack_require__.d(exports, "GoalCreateModule", function() { return GoalCreateModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -22,31 +22,31 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
-var DraftsModule = (function () {
-    function DraftsModule() {
+var GoalCreateModule = (function () {
+    function GoalCreateModule() {
     }
-    DraftsModule = __decorate([
-        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["u" /* NgModule */])({
+    GoalCreateModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
             imports: [
                 __WEBPACK_IMPORTED_MODULE_1__angular_common__["b" /* CommonModule */],
-                __WEBPACK_IMPORTED_MODULE_3__draft_routing__["a" /* DraftRouting */]
+                __WEBPACK_IMPORTED_MODULE_3__goal_create_routing__["a" /* GoalCreateRouting */]
             ],
-            declarations: [__WEBPACK_IMPORTED_MODULE_2__drafts_component__["a" /* DraftsComponent */]]
+            declarations: [__WEBPACK_IMPORTED_MODULE_2__goal_create_component__["a" /* GoalCreateComponent */]]
         }), 
         __metadata('design:paramtypes', [])
-    ], DraftsModule);
-    return DraftsModule;
+    ], GoalCreateModule);
+    return GoalCreateModule;
 }());
-//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/drafts.module.js.map
+//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/goal-create.module.js.map
 
 /***/ },
 
-/***/ 782:
+/***/ 829:
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return DraftsComponent; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return GoalCreateComponent; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -57,54 +57,54 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
-var DraftsComponent = (function () {
-    function DraftsComponent() {
+var GoalCreateComponent = (function () {
+    function GoalCreateComponent() {
     }
-    DraftsComponent.prototype.ngOnInit = function () {
+    GoalCreateComponent.prototype.ngOnInit = function () {
     };
-    DraftsComponent = __decorate([
-        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["N" /* Component */])({
-            selector: 'app-drafts',
-            template: __webpack_require__(835),
-            styles: [__webpack_require__(817)]
+    GoalCreateComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+            selector: 'app-goal-create',
+            template: __webpack_require__(881),
+            styles: [__webpack_require__(871)]
         }), 
         __metadata('design:paramtypes', [])
-    ], DraftsComponent);
-    return DraftsComponent;
+    ], GoalCreateComponent);
+    return GoalCreateComponent;
 }());
-//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/drafts.component.js.map
+//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/goal-create.component.js.map
 
 /***/ },
 
-/***/ 800:
+/***/ 849:
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_router__ = __webpack_require__(67);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__drafts_component__ = __webpack_require__(782);
-/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return DraftRouting; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__goal_create_component__ = __webpack_require__(829);
+/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return GoalCreateRouting; });
 
 
 // import { IdeasCategoryComponent }  from '../ideas-category/ideas-category.component';
-var DraftRoutes = [
-    { path: '', component: __WEBPACK_IMPORTED_MODULE_1__drafts_component__["a" /* DraftsComponent */] }
+var GoalCreateRoutes = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_1__goal_create_component__["a" /* GoalCreateComponent */] }
 ];
-var DraftRouting = __WEBPACK_IMPORTED_MODULE_0__angular_router__["b" /* RouterModule */].forChild(DraftRoutes);
-//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/draft-routing.js.map
+var GoalCreateRouting = __WEBPACK_IMPORTED_MODULE_0__angular_router__["b" /* RouterModule */].forChild(GoalCreateRoutes);
+//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/goal-create-routing.js.map
 
 /***/ },
 
-/***/ 817:
+/***/ 871:
 /***/ function(module, exports) {
 
 module.exports = ""
 
 /***/ },
 
-/***/ 835:
+/***/ 881:
 /***/ function(module, exports) {
 
-module.exports = "<p>\n  drafts works!\n</p>\n"
+module.exports = "<p>\n  goal-create works!\n</p>\n"
 
 /***/ }
 
--- web/dist/8.chunk.js
+++ web/dist/8.chunk.js
@@ -1,13 +1,14 @@
 webpackJsonp([8,13],{
 
-/***/ 780:
+/***/ 792:
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__(80);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__settings_component__ = __webpack_require__(808);
-/* harmony export (binding) */ __webpack_require__.d(exports, "SettingsModule", function() { return SettingsModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__drafts_component__ = __webpack_require__(828);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__draft_routing__ = __webpack_require__(848);
+/* harmony export (binding) */ __webpack_require__.d(exports, "DraftsModule", function() { return DraftsModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -20,30 +21,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
-var SettingsModule = (function () {
-    function SettingsModule() {
+
+var DraftsModule = (function () {
+    function DraftsModule() {
     }
-    SettingsModule = __decorate([
-        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["u" /* NgModule */])({
+    DraftsModule = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
             imports: [
-                __WEBPACK_IMPORTED_MODULE_1__angular_common__["b" /* CommonModule */]
+                __WEBPACK_IMPORTED_MODULE_1__angular_common__["b" /* CommonModule */],
+                __WEBPACK_IMPORTED_MODULE_3__draft_routing__["a" /* DraftRouting */]
             ],
-            declarations: [__WEBPACK_IMPORTED_MODULE_2__settings_component__["a" /* SettingsComponent */]]
+            declarations: [__WEBPACK_IMPORTED_MODULE_2__drafts_component__["a" /* DraftsComponent */]]
         }), 
         __metadata('design:paramtypes', [])
-    ], SettingsModule);
-    return SettingsModule;
+    ], DraftsModule);
+    return DraftsModule;
 }());
-//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/settings.module.js.map
+//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/drafts.module.js.map
 
 /***/ },
 
-/***/ 808:
+/***/ 828:
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return SettingsComponent; });
+/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return DraftsComponent; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -54,36 +57,54 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
-var SettingsComponent = (function () {
-    function SettingsComponent() {
+var DraftsComponent = (function () {
+    function DraftsComponent() {
     }
-    SettingsComponent.prototype.ngOnInit = function () {
+    DraftsComponent.prototype.ngOnInit = function () {
     };
-    SettingsComponent = __decorate([
-        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["N" /* Component */])({
-            selector: 'app-settings',
-            template: __webpack_require__(844),
-            styles: [__webpack_require__(826)]
+    DraftsComponent = __decorate([
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+            selector: 'app-drafts',
+            template: __webpack_require__(880),
+            styles: [__webpack_require__(870)]
         }), 
         __metadata('design:paramtypes', [])
-    ], SettingsComponent);
-    return SettingsComponent;
+    ], DraftsComponent);
+    return DraftsComponent;
 }());
-//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/settings.component.js.map
+//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/drafts.component.js.map
 
 /***/ },
 
-/***/ 826:
+/***/ 848:
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_router__ = __webpack_require__(67);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__drafts_component__ = __webpack_require__(828);
+/* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return DraftRouting; });
+
+
+// import { IdeasCategoryComponent }  from '../ideas-category/ideas-category.component';
+var DraftRoutes = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_1__drafts_component__["a" /* DraftsComponent */] }
+];
+var DraftRouting = __WEBPACK_IMPORTED_MODULE_0__angular_router__["b" /* RouterModule */].forChild(DraftRoutes);
+//# sourceMappingURL=/var/www/html/bucketlist/bucketlist/src/draft-routing.js.map
+
+/***/ },
+
+/***/ 870:
 /***/ function(module, exports) {
 
 module.exports = ""
 
 /***/ },
 
-/***/ 844:
+/***/ 880:
 /***/ function(module, exports) {
 
-module.exports = "<p>\n  settings works!\n</p>\n"
+module.exports = "<p>\n  drafts works!\n</p>\n"
 
 /***/ }
 
