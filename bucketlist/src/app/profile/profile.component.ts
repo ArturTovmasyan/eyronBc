a299950b88e4c6ef3a2eec74ca13a85155fb49a8
Ani <animanukyan23@gmail.com>
2017-01-09 16:52:22 +0400

    profile page styles

--- bucketlist/src/app/profile/profile.component.ts
+++ bucketlist/src/app/profile/profile.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer} from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.less']
 })
+
 export class ProfileComponent implements OnInit {
   @ViewChild("tooltip") public tooltipElementRef: ElementRef;
   public hoveredText:string;
