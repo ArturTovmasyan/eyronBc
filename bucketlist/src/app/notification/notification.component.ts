55b96fa1104d6f7f54320a48b53f071404e3f5c8
ando94-23 <ando94-23@mail.ru>
2017-01-12 18:18:23 +0400

    fix notification page

--- bucketlist/src/app/notification/notification.component.ts
+++ bucketlist/src/app/notification/notification.component.ts
@@ -13,12 +13,18 @@ export class NotificationComponent implements OnInit {
   public notifications: any[];
   public count: number;
   public errorMessage:string;
+  public busy: boolean = false;
+  public reserve: any[];
+  public serverPath:string = '';
+  public time: any[];
 
   constructor(private _projectService: ProjectService) {
   }
 
   ngOnInit() {
+    this.serverPath = this._projectService.getPath();
     this.getNotifications();
+
   }
 
 
@@ -30,10 +36,43 @@ export class NotificationComponent implements OnInit {
               this.notifications = notify.userNotifications;
               this.count = notify.unreadCount;
 
+                this.setReserve();
+                console.log(this.notifications.length);
 
             },
-            
             error => this.errorMessage = <any>error);
     
   }
+  setReserve(){
+    this._projectService.getNotifications()
+        .subscribe(
+            notify => {
+                this.reserve = notify.userNotifications;
+                this.busy = false;
+            }
+
+          )
+
+  }
+
+
+  getReserve(){
+    this.notifications = this.notifications.concat(this.reserve);
+    this.setReserve();
+  }
+
+  onScroll(){
+      if(this.busy || !this.reserve || !this.reserve.length)return;
+      this.busy = true;
+      this.getReserve();
+    }
+  bodyInHtml(body) {
+    let words = body.split(" "),
+        lastWord = words[words.length -1];
+    return body.slice(0, -1 * lastWord.length) + "<a>"+ lastWord + "</a>";
+  };
+    delete(id, index){
+    this.notifications.splice(index,1);
+};
+
 }
