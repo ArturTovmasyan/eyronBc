b0e62a46ca4cfe4b772f2711d387cbd20d090702
Ani <animanukyan23@gmail.com>
2017-01-12 14:49:27 +0400

    settings profile

--- bucketlist/src/app/settings/settings.component.ts
+++ bucketlist/src/app/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes, ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import {ProjectService} from '../project.service';
 import {CacheService, CacheStoragesEnum} from 'ng2-cache/ng2-cache';
+import {TranslateService} from 'ng2-translate';
 import { Broadcaster } from '../tools/broadcaster';
 
 @Component({
@@ -12,8 +13,21 @@ import { Broadcaster } from '../tools/broadcaster';
 export class SettingsComponent implements OnInit {
   public eventId: number = 0;
   public type: string;
+  public languages: any[] = [
+      {
+          value:'en',
+          name: 'English'
+      },
+      {
+          value:'ru',
+          name: 'Russian'
+      }
+  ];
+  public currentLang: string;
+
 
   constructor(
+      private _translate: TranslateService,
       private route: ActivatedRoute,
       private _projectService: ProjectService,
       private _cacheService: CacheService,
@@ -34,6 +48,6 @@ export class SettingsComponent implements OnInit {
   }
 
   getUserInfoByType(){
-    
+    this.currentLang = this._translate.currentLang;
   }
 }
