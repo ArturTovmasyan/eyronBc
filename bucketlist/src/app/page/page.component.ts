93d3d124bb4c5ef23111821614581130093fb4e4
aram simonyan <asimonyan87@gmail.com>
2017-01-14 12:50:05 +0400

    Fix in contact-us form

--- bucketlist/src/app/page/page.component.ts
+++ bucketlist/src/app/page/page.component.ts
@@ -32,6 +32,15 @@ export class PageComponent implements OnInit {
       if(this.eventId != val.id && val instanceof NavigationEnd){
         this.eventId = val.id;
         this.name = this.route.snapshot.params['name'] ? this.route.snapshot.params['name'] : 'how-it-works';
+
+          if(this.name == 'contact-us') {
+              this.isSend = false;
+              this.emailData.fullName = '';
+              this.emailData.email = '';
+              this.emailData.subject = '';
+              this.emailData.message = '';
+          }
+
         this.getPage(this.name, this.locale);
       }
     });
@@ -48,27 +57,46 @@ export class PageComponent implements OnInit {
   }
 
   getPage(name, locale){
-    this._projectService.getPage(name, locale)
-        .subscribe(
-            data => {
-                this.data = data[0];
-                this.description = this.data.description;
-                this.title = this.data.title;
-            });
+
+      this._projectService.getPage(name, locale)
+          .subscribe(
+              data => {
+                  this.data = data[0];
+                  this.description = this.data.description;
+                  this.title = this.data.title;
+              });
   }
 
-  sendEmail(emailData) {
-    this.emailData.fullName = emailData.fullName;
-    this.emailData.email = emailData.email;
-    this.emailData.subject = emailData.subject;
-    this.emailData.message = emailData.message;
+  checkFormValue() {
 
-    this._projectService.sendEmail(this.emailData)
-        .subscribe(
-            () => {
-                this.isSend = true;
-                console.log(this.isSend);
-            }
-          );
+      if(this.emailData.fullName.length > 0 &&
+          this.emailData.email.length > 0 &&
+          this.emailData.subject.length > 0 &&
+          this.emailData.message.length > 0) {
+          return true;
+      }
+      return false;
   }
+
+    sendEmail(emailData) {
+
+        if(this.checkFormValue()) {
+
+            this.emailData.fullName = emailData.fullName;
+            this.emailData.email = emailData.email;
+            this.emailData.subject = emailData.subject;
+            this.emailData.message = emailData.message;
+
+            this._projectService.sendEmail(this.emailData)
+                .subscribe(
+                    () => {
+                        this.isSend = true;
+                        // this.form.reset();
+                    }
+                );
+        }
+        else{
+            return false;
+        }
+    }
 }
