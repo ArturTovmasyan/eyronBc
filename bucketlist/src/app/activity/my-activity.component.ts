c26248da8cdd365ac9d8641faed42f093b916b19
Ani <animanukyan23@gmail.com>
2017-01-18 11:54:39 +0400

    chnages

--- bucketlist/src/app/activity/my-activity.component.ts
+++ bucketlist/src/app/activity/my-activity.component.ts
@@ -53,7 +53,7 @@ export class MyActivityComponent implements OnInit {
 
         this.interval = setInterval(() => {
             if(this.Activities && !this.single){
-                this._projectService.getActivities(0, this.count, this.Activities[0].datetime).subscribe(
+                this._projectService.getActivities(0, this.count, this.userId, this.Activities[0].datetime).subscribe(
                     data => {
                         if(data && data.length != 0){
                             this.newData = data;
@@ -150,7 +150,7 @@ export class MyActivityComponent implements OnInit {
 
     newActivityFn() {
         if(this.Activities && !this.single){
-            this._projectService.getActivities(0, this.count, this.Activities[0].datetime).subscribe(
+            this._projectService.getActivities(0, this.count, this.userId, this.Activities[0].datetime).subscribe(
             data => {
                  if(data && data.length != 0){
                         this.newData = data;
