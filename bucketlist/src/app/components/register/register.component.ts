0dc837f4370a45e84517e4b9b9d7bc6749cbc989
ARTUR <artur.tovmasyan777@gmail.com>
2017-01-20 13:30:57 +0400

    add date field value in registartion page STEP 1

--- bucketlist/src/app/components/register/register.component.ts
+++ bucketlist/src/app/components/register/register.component.ts
@@ -3,45 +3,79 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ValidationService } from 'app/validation.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.less']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.less']
 })
 export class RegisterComponent implements OnInit {
 
-  registerData:any;
+    registerData:any;
+    arrayMonth:string[] = [];
+    arrayDay:number[] = [];
+    arrayYear:number[] = [];
 
-  constructor(private formBuilder: FormBuilder)
-  {
+    constructor(private formBuilder: FormBuilder)
+    {
+        // this.registerData = {
+        //   file: '',
+        //   firstName: '',
+        //   lastName: '',
+        //   email: '',
+        //   passwordFirst: '',
+        //   passwordSecond: '',
+        // };
 
-    // this.registerData = {
-    //   file: '',
-    //   firstName: '',
-    //   lastName: '',
-    //   email: '',
-    //   passwordFirst: '',
-    //   passwordSecond: '',
-    // };
+        this.arrayMonth = [
+            // 'form.birth_date_month',
+            'form.month_january',
+            'form.month_february',
+            'form.month_march',
+            'form.month_april',
+            'form.month_may',
+            'form.month_june',
+            'form.month_july',
+            'form.month_august',
+            'form.month_september',
+            'form.month_october',
+            'form.month_november',
+            'form.month_december'
+        ];
 
-    this.registerData = this.formBuilder.group({
-      'file': ['', null],
-      'firstName': ['', [Validators.required]],
-      'lastName': ['', [Validators.required]],
-      'email': ['', [Validators.required, ValidationService.emailValidator]],
-      'passwordFirst': ['', [Validators.required, Validators.minLength(6), ValidationService.passwordValidator]],
-      'passwordSecond' : ['', [Validators.required, Validators.minLength(6),ValidationService.passwordValidator]]
-    });
-  }
+        this.createDays(31);
+        this.createYears(1917, 2017);
 
-  ngOnInit() {
-  }
+        this.registerData = this.formBuilder.group({
+            'file': ['', null],
+            'firstName': ['', [Validators.required]],
+            'lastName': ['', [Validators.required]],
+            'email': ['', [Validators.required, ValidationService.emailValidator]],
+            'passwordFirst': ['', [Validators.required, Validators.minLength(6), ValidationService.passwordValidator]],
+            'passwordSecond' : ['', [Validators.required, Validators.minLength(6),ValidationService.passwordValidator]],
+            'month' : ['', null]
+        });
+    }
 
-  /**
-   *
-   * @param registerData
-   */
-  sendRegisterData(registerData)
-  {
-    console.log(registerData);
-  }
+    ngOnInit() {
+    }
+
+    createDays(number) {
+        for (let i = 1; i <= number; i++) {
+            this.arrayDay.push(i);
+        }
+    }
+
+    createYears(number1, number2) {
+        for (let i = number2; i>= number1; i--) {
+            this.arrayYear.push(i);
+        }
+    }
+
+    /**
+     *
+     * @param registerData
+     */
+    sendRegisterData(registerData)
+    {
+        // console.log(registerData.month);
+    }
 }
