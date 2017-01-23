51f6f378dca66b364f142ae26ae83f775be2cc79
gevor <gsmgevor.gs@gmail.com>
2017-01-23 12:35:38 +0400

    build

--- bucketlist/src/app/components/profile-goal/profile-goal.component.ts
+++ bucketlist/src/app/components/profile-goal/profile-goal.component.ts
@@ -75,5 +75,28 @@ export class ProfileGoalComponent implements OnInit {
 
     return (d1 < d2);
   };
+
+  manageGoal(id){
+    this._projectService.getUserGoal(id).subscribe((data) => {
+      this.broadcaster.broadcast('addModal', data);
+    });
+  }
+
+  completeGoal(id, isManage){
+    this.success = true;
+    
+    if(isManage){
+      this._projectService.getStory(id).subscribe((data)=> {
+        this.broadcaster.broadcast('doneModal', data);
+      })
+    } else {
+      this._projectService.setDoneUserGoal(id).subscribe(() => {
+        this._projectService.getStory(id).subscribe((data)=> {
+          this.broadcaster.broadcast('doneModal', data);
+        })
+      });
+    }
+    
+  }
   
 }
