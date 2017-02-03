ff14262e1c7907e7371547adda62dc962bf760dc
Ani <animanukyan23@gmail.com>
2017-02-03 11:34:24 +0400

    styles

--- bucketlist/src/app/goal-create/goal-create.component.ts
+++ bucketlist/src/app/goal-create/goal-create.component.ts
@@ -41,7 +41,7 @@ export class GoalCreateComponent implements OnInit {
         observer: true,
         autoHeight: true,
         loop: false,
-        slidesPerView: 3,
+        slidesPerView: (window.innerWidth < 768?1:(window.innerWidth < 992?2:3)),
         nextButton: '.icon-arrow-right',
         prevButton: '.icon-arrow-left',
         spaceBetween: 30
