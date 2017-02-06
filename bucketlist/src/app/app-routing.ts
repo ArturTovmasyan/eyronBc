2962fb689a02328c2d3668e4f4c7911550def90d
ARTUR <artur.tovmasyan777@gmail.com>
2017-02-06 18:36:55 +0400

    Confirm registration pagess

--- bucketlist/src/app/app-routing.ts
+++ bucketlist/src/app/app-routing.ts
@@ -29,6 +29,8 @@ const appRoutes: Routes = [
   { path: 'ideas', loadChildren: './ideas/ideas.module#IdeasModule'},
   { path: '', component: DashboardComponent },
   { path: 'error', component: ErrorComponent },
+  { path: 'resend-message', component: ErrorComponent },
+  { path: 'update-email', component: ErrorComponent },
   { path: '**', component: ErrorComponent }
 ];
 
