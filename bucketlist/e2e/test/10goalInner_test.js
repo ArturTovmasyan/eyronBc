a2604ed50583ce90ea023cf78746868e7a57af79
ARTUR <artur.tovmasyan777@gmail.com>
2017-03-15 17:30:54 +0400

    fix

---
+++ bucketlist/e2e/test/10goalInner_test.js
@@ -0,0 +1,26 @@
+/// <reference path="../steps.d.ts" />
+Feature('Test goal inner page');
+
+BeforeSuite((I) => {
+  I.resizeWindow('maximize');
+});
+
+Scenario('Test goal inner page', (I) => {
+
+  I.amOnPage('/');
+  I.see('JOIN');
+  I.click('JOIN');
+  I.loginUser('user1@user.com', 'Test1234');
+  I.seeCurrentUrlEquals('/activity');
+  I.wait(1);
+  I.amOutsideAngularApp();
+  I.click('a.user-popover');
+  I.click('My Bucketlist');
+  I.waitForText('user1 useryan', 5);
+  I.amOnPage('/goal/goal9');
+  I.waitForText('One must be a fox in order to recognize traps, and a lion to frighten off wolves.');
+  I.executeScript('window.scrollTo(0, document.body.scrollHeight);');
+  I.fillField('commentBody', 'My first comment added');
+  I.pressEnterOnComment();
+  I.wait(2);
+});
